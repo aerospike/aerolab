05d310a8b7e84b052decf3d3be8710faf0867a37
Ken Tune <ktune@aerospike.com>
2020-07-21 15:13:15 +0100

    Updated for Aerospike V5

--- kafka-connect/cluster-setup/make-cluster.sh
+++ kafka-connect/cluster-setup/make-cluster.sh
@@ -1,6 +1,5 @@
 CONFIG_FILE=kafka-connect.conf
 CLUSTER_NAME=kafka-connect
-CREDENTIALS=credentials.conf
-VERSION=4.6.0.4
+VERSION=5.0.0.8
 
-aerolab make-cluster --config=`pwd`/$CREDENTIALS -n $CLUSTER_NAME -m mesh -c 3 -o `pwd`/$CONFIG_FILE -f `pwd`/features.conf -v $VERSION
+aerolab make-cluster -n $CLUSTER_NAME -m mesh -c 3 -o `pwd`/$CONFIG_FILE -f `pwd`/features.conf -v $VERSION
