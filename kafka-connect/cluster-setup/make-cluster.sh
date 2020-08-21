5feddf2e053140e2ac9ecc88b73d187fe8dc801a
Ken Tune <ktune@aerospike.com>
2020-08-21 14:42:05 +0000

    Library problem requires earlier version of Aerospike

--- kafka-connect/cluster-setup/make-cluster.sh
+++ kafka-connect/cluster-setup/make-cluster.sh
@@ -1,5 +1,5 @@
 CONFIG_FILE=kafka-connect.conf
 CLUSTER_NAME=kafka-connect
-VERSION=5.0.0.8
+VERSION=5.0.0.3
 
 aerolab make-cluster -n $CLUSTER_NAME -m mesh -c 3 -o `pwd`/$CONFIG_FILE -f `pwd`/features.conf -v $VERSION
