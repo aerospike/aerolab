52c55fead4bbf0768b7801b3014fbceaad8fd15e
Robert Glonek <rglonek@Roberts-MacBook-Pro.local>
2020-09-28 16:25:07 +0100

    cleanup

--- kafka-connect/cluster-setup/make-cluster.sh
+++
@@ -1,5 +0,0 @@
-CONFIG_FILE=kafka-connect.conf
-CLUSTER_NAME=kafka-connect
-VERSION=5.0.0.3
-
-aerolab make-cluster -n $CLUSTER_NAME -m mesh -c 3 -o `pwd`/$CONFIG_FILE -f `pwd`/features.conf -v $VERSION
