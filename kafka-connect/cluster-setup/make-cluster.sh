902d496468a1968a9fde68ec2281e8c7ecf97eaa
Ken Tune <ktune@aerospike.com>
2021-06-09 12:55:40 +0100

    Update version of Aerospike used for Kafka Connect to 5.6.0.5

--- kafka-connect/cluster-setup/make-cluster.sh
+++ kafka-connect/cluster-setup/make-cluster.sh
@@ -1,5 +1,5 @@
 CONFIG_FILE=kafka-connect.conf
 CLUSTER_NAME=kafka-connect
-VERSION=5.0.0.3
+VERSION=5.6.0.5
 
 aerolab make-cluster -n $CLUSTER_NAME -m mesh -c 3 -o `pwd`/$CONFIG_FILE -f `pwd`/features.conf -v $VERSION
