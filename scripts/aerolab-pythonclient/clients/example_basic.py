7629bef3b0602cc4ceacdc20c9abab9c3dedda3a
ajohnson@aerospike.com <ajohnson@aerospike.com>
2022-04-08 10:05:25 +0100

    Added in basic/tls python code

---
+++ scripts/aerolab-pythonclient/clients/example_basic.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+# import the module
+from __future__ import print_function
+import aerospike
+import time
+
+#######################################################################################
+## This code authenticates with the database, writes a record and then reads it back ##
+#######################################################################################
+
+# Modify CLUSTERIP to be the IP of one of your database nodes
+
+
+# Configure the client CLUSTERIP:3000
+config = {
+  'hosts': [ ('CLUSTERIP', 3000) ],
+  'policies': {
+      'timeout': 1000,
+      'auth_mode': aerospike.AUTH_EXTERNAL_INSECURE
+  }
+}
+
+# Create a client and connect it to the cluster
+print("Connecting/Authenticating")
+try:
+  client = aerospike.client(config).connect()
+except:
+  import sys
+  print("failed to connect to the cluster with", config['hosts'])
+  sys.exit(1)
+
+print("Ready to Read/Write")
+var=input("Press Enter")
+print("")
+key = ('test', 'demo', 'key1')
+
+# Records are addressable via a tuple of (namespace, set, key)
+print("Writing Key : %s" %(key,))
+try:
+  # Write a record
+  client.put(key, { 'name': 'John Doe', 'age': 50 })
+except Exception as e:
+  import sys
+  print("error: {0}".format(e), file=sys.stderr)
+  time.sleep(1)
+  sys.exit(1)
+
+print("")
+
+# Read a record
+print("Reading Key : %s" %(key,))
+try:
+  (key, metadata, record) = client.get(key)
+except Exception as e:
+  import sys
+  print("error: {0}".format(e), file=sys.stderr)
+  time.sleep(1)
+  sys.exit(1)
+
+print("Record : %s" % (record,))
+#  time.sleep(.1)
+
+# Close the connection to the Aerospike cluster
+client.close()
