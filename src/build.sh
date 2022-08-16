83f2be7fef2ab47bd47244d3d3ff535f62eb5b33
Robert Glonek <rglonek@aerospike.com>
2022-08-16 11:45:38 -0700

    cgo min macos version lock

--- src/build.sh
+++ src/build.sh
@@ -13,7 +13,7 @@ mkdir -p ../bin || exit 1
 # compile
 MPATH=$(pwd)
 env GOOS=linux GOARCH=amd64 go build -gcflags=-trimpath=${MPATH} -asmflags=-trimpath=${MPATH} -ldflags="-s -w" -o ../bin/aerolab-linux || exit 1
-env GOOS=darwin GOARCH=amd64 go build -gcflags=-trimpath=${MPATH} -asmflags=-trimpath=${MPATH} -ldflags="-s -w" -o ../bin/aerolab-macos || exit 1
+env GOOS=darwin GOARCH=amd64 CGO_CFLAGS="-mmacosx-version-min=11.3" CGO_LDFLAGS="-mmacosx-version-min=11.3" go build -gcflags=-trimpath=${MPATH} -asmflags=-trimpath=${MPATH} -ldflags="-s -w" -o ../bin/aerolab-macos || exit 1
 
 # pack
 cd ../bin || exit 1
