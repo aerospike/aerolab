0d10197354db6bdc3f0cfe65d643060dabd1fa1e
Robert Glonek <rglonek@aerospike.com>
2022-11-22 15:56:41 -0800

    openvpn-tunner arm support

--- tunnel-container-openvpn/build-run/build.sh
+++ tunnel-container-openvpn/build-run/build.sh
@@ -1,1 +1,5 @@
+TREPO="openvpn-base"
+uname -p |grep arm >/dev/null 2>&1 && TREPO="openvpn-base-arm"
+uname -m |grep arm >/dev/null 2>&1 && TREPO="openvpn-base-arm"
+sed "s/TREPO/bestmethod\/${TREPO}/g" Dockerfile.template > Dockerfile
 docker build -t openvpn:1 .
