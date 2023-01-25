d9547978ed2c073a8877e47acbd9ae1befe8af7a
Robert Glonek <rglonek@aerospike.com>
2023-01-24 16:44:28 -0800

    gocui deps for editor

--- src/confeditor/editor.go
+++ src/confeditor/editor.go
@@ -1,12 +1,45 @@
 package confeditor
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/jroimartin/gocui"
+)
 
 type Editor struct {
 	Path string
 }
 
 func (e *Editor) Run() error {
-	fmt.Println("Hello Bob World")
+	g, err := gocui.NewGui(gocui.OutputNormal)
+	if err != nil {
+		return err
+	}
+	defer g.Close()
+
+	g.SetManagerFunc(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		return err
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+		return err
+	}
 	return nil
+}
+
+func layout(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
+	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(v, "Hello world!")
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.ErrQuit
 }
--- src/go.mod
+++ src/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/aerospike/aerospike-client-go/v6 v6.6.0
 	github.com/aws/aws-sdk-go v1.44.158
 	github.com/bestmethod/inslice v0.0.0-20210212091431-146fa4d769bf
+	github.com/jroimartin/gocui v0.5.0
 	github.com/rglonek/jeddevdk-goflags v0.0.0-20221012221225-0a7df0798e9f
 	golang.org/x/crypto v0.3.0
 	golang.org/x/term v0.2.0
@@ -15,6 +16,8 @@ require (
 
 require (
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
 	github.com/yuin/gopher-lua v0.0.0-20220504180219-658193537a64 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
