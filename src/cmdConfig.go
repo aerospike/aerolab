b18e7fef45bceb46b5e53ef817de0962170ca100
Robert Glonek <rglonek@aerospike.com>
2023-02-01 09:22:38 -0800

    list subnets and security groups

--- src/cmdConfig.go
+++ src/cmdConfig.go
@@ -31,6 +31,8 @@ type configAwsCmd struct {
 	DestroySecGroups destroySecGroupsCmd `command:"delete-security-groups" subcommands-optional:"true" description:"delete aerolab-managed security groups"`
 	LockSecGroups    lockSecGroupsCmd    `command:"lock-security-groups" subcommands-optional:"true" description:"lock the client security groups so that AMS/vscode are only accessible from a set IP"`
 	CreateSecGroups  createSecGroupsCmd  `command:"create-security-groups" subcommands-optional:"true" description:"create AeroLab-managed security groups in a given VPC"`
+	ListSecGroups    listSecGroupsCmd    `command:"list-security-groups" subcommands-optional:"true" description:"list current aerolab-managed security groups"`
+	ListSubnets      listSubnetsCmd      `command:"list-subnets" subcommands-optional:"true" description:"list VPCs and subnets in the current region"`
 	Help             helpCmd             `command:"help" subcommands-optional:"true" description:"Print help"`
 }
 
@@ -38,6 +40,14 @@ func (c *configAwsCmd) Execute(args []string) error {
 	a.parser.WriteHelp(os.Stderr)
 	os.Exit(1)
 	return nil
+}
+
+type listSecGroupsCmd struct {
+	Help helpCmd `command:"help" subcommands-optional:"true" description:"Print help"`
+}
+
+type listSubnetsCmd struct {
+	Help helpCmd `command:"help" subcommands-optional:"true" description:"Print help"`
 }
 
 type destroySecGroupsCmd struct {
@@ -71,6 +81,34 @@ type configDefaultsCmd struct {
 	Value       flags.Filename `short:"v" long:"value" description:"Value to set" default:""`
 	Reset       bool           `short:"r" long:"reset" description:"Reset to default value. Use instead of --value"`
 	Help        helpCmd        `command:"help" subcommands-optional:"true" description:"Print help"`
+}
+
+func (c *listSecGroupsCmd) Execute(args []string) error {
+	if earlyProcess(args) {
+		return nil
+	}
+	if a.opts.Config.Backend.Type != "aws" {
+		return logFatal("required backend type to be AWS")
+	}
+	err := b.ListSecurityGroups()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *listSubnetsCmd) Execute(args []string) error {
+	if earlyProcess(args) {
+		return nil
+	}
+	if a.opts.Config.Backend.Type != "aws" {
+		return logFatal("required backend type to be AWS")
+	}
+	err := b.ListSubnets()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (c *createSecGroupsCmd) Execute(args []string) error {
