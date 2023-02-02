cdc58147b45f5754428a69e09a082af7f75d5d6c
Robert Glonek <rglonek@aerospike.com>
2023-02-01 20:20:51 -0800

    filter selector design

--- src/cmdClusterPartition.go
+++ src/cmdClusterPartition.go
@@ -21,24 +21,27 @@ func (c *clusterPartitionCmd) Execute(args []string) error {
 
 /*
 # partition disks
-aerolab cluster partition create -n name -l all --filters="" --partitions="20%,20%,20%,20%,20%"
+aerolab cluster partition create -n name -l all --filter-disks=1-3 --filter-partitions=1-3 --filter-type=nvme --partitions="20%,20%,20%,20%,20%"
 
 # mkfs particular partitions, and mount
-aerolab cluster partition mkfs   -n name -l all --filters="" --mount-root="/mnt/" --type=xfs --options="noatime"
+aerolab cluster partition mkfs   -n name -l all --filter-disks=1-3 --filter-partitions=1-3 --filter-type=nvme --mount-root="/mnt/" --type=xfs --options="noatime"
 
 # clear namespace storage type definitions from the conf file
 aerolab cluster partition conf   -n name -l all --clear=storage
 aerolab cluster partition conf   -n name -l all --clear=allflash
 
 # add storage type device and setup the devices matching filters
-aerolab cluster partition conf   -n name -l all --filters="" --namespace=test --device
+aerolab cluster partition conf   -n name -l all --filter-disks=1-3 --filter-partitions=1-3 --filter-type=nvme --namespace=test --device
 
 # add shadow devices matching filters
-aerolab cluster partition conf   -n name -l all --filters="" --namespace=test --shadow
+aerolab cluster partition conf   -n name -l all --filter-disks=1-3 --filter-partitions=1-3 --filter-type=nvme --namespace=test --shadow
 
 # add allflash definition matching filters
-aerolab cluster partition conf   -n name -l all --filters="" --namespace=test --allflash
+aerolab cluster partition conf   -n name -l all --filter-disks=1-3 --filter-partitions=1-3 --filter-type=nvme --namespace=test --allflash
 
 --- filters ---
 TODO
+--filter-disks=1-3      // first 3 nvme, or first 3 sdX, that are not used for root '/', leave empty for ALL disks
+--filter-partitions=1-3 // first 3 partitions on each selected disk, or don't set to select the disk itself
+--filter-type=nvme      // or ebs
 */
