67cd881aa5f2d98660786765a82ce84117f15502
Robert Glonek <rglonek@aerospike.com>
2023-02-01 20:31:39 -0800

    partitioner proposal built

--- src/cmdClusterPartition.go
+++ src/cmdClusterPartition.go
@@ -40,8 +40,24 @@ aerolab cluster partition conf   -n name -l all --filter-disks=1-3 --filter-part
 aerolab cluster partition conf   -n name -l all --filter-disks=1-3 --filter-partitions=1-3 --filter-type=nvme --namespace=test --allflash
 
 --- filters ---
-TODO
 --filter-disks=1-3      // first 3 nvme, or first 3 sdX, that are not used for root '/', leave empty for ALL disks
 --filter-partitions=1-3 // first 3 partitions on each selected disk, or don't set to select the disk itself
 --filter-type=nvme      // or ebs
+
+--- example with 2 nvme, 2 ebs, and trying to use allflash (5 partitions, one for allflash on nvme only) ---
+# all nvme 5 partitions
+aerolab cluster partition create -n name -l all --filter-type=nvme --partitions="20%,20%,20%,20%,20%"
+# all ebs 4 partitions
+aerolab cluster partition create -n name -l all --filter-type=ebs --partitions="25%,25%,25%,25%"
+# all nvme mkfs and mount first partition for allflash
+aerolab cluster partition mkfs   -n name -l all --filter-partitions=1 --filter-type=nvme --mount-root="/mnt/" --type=xfs --options="noatime"
+# clear configs in aerospike.conf for storage engine devices
+aerolab cluster partition conf   -n name -l all --namespace=test --clear=storage
+aerolab cluster partition conf   -n name -l all --namespace=test --clear=allflash
+# add partitions 2-5 of all nvme as devices to aerospike.conf
+aerolab cluster partition conf   -n name -l all --filter-partitions=2-5 --filter-type=nvme --namespace=test --device
+# add partitions 1-4 of all ebs as shadow to aerospike.conf
+aerolab cluster partition conf   -n name -l all --filter-partitions=1-4 --filter-type=ebs --namespace=test --shadow
+# add partition 1 of all nvme as allflash
+aerolab cluster partition conf   -n name -l all --filter-partitions=1 --filter-type=nvme --namespace=test --allflash
 */
