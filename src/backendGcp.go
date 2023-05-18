1a9c43308e89e5700d8c76b5056c8b360634f4b9
Robert Glonek <rglonek@aerospike.com>
2023-05-18 13:35:08 -0700

    gcp wip

--- src/backendGcp.go
+++ src/backendGcp.go
@@ -21,7 +21,6 @@ import (
 	compute "cloud.google.com/go/compute/apiv1"
 	"cloud.google.com/go/compute/apiv1/computepb"
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/bestmethod/inslice"
 	"golang.org/x/crypto/ssh"
 	"google.golang.org/api/iterator"
@@ -454,6 +453,12 @@ type instanceDetail struct {
 }
 
 func (d *backendGcp) getInstanceDetails(name string, nodes []int) (zones map[int]instanceDetail, err error) {
+	if len(nodes) == 0 {
+		nodes, err = d.NodeListInCluster(name)
+		if err != nil {
+			return nil, fmt.Errorf("could not get IP list\n%s", err)
+		}
+	}
 	zones = make(map[int]instanceDetail)
 	ctx := context.Background()
 	instancesClient, err := compute.NewInstancesRESTClient(ctx)
@@ -498,6 +503,13 @@ func (d *backendGcp) getInstanceDetails(name string, nodes []int) (zones map[int
 }
 
 func (d *backendGcp) ClusterStart(name string, nodes []int) error {
+	var err error
+	if len(nodes) == 0 {
+		nodes, err = d.NodeListInCluster(name)
+		if err != nil {
+			return fmt.Errorf("could not get IP list\n%s", err)
+		}
+	}
 	zones, err := d.getInstanceDetails(name, nodes)
 	if err != nil {
 		return err
@@ -531,6 +543,13 @@ func (d *backendGcp) ClusterStart(name string, nodes []int) error {
 }
 
 func (d *backendGcp) ClusterStop(name string, nodes []int) error {
+	var err error
+	if len(nodes) == 0 {
+		nodes, err = d.NodeListInCluster(name)
+		if err != nil {
+			return fmt.Errorf("could not get IP list\n%s", err)
+		}
+	}
 	zones, err := d.getInstanceDetails(name, nodes)
 	if err != nil {
 		return err
@@ -564,6 +583,13 @@ func (d *backendGcp) ClusterStop(name string, nodes []int) error {
 }
 
 func (d *backendGcp) ClusterDestroy(name string, nodes []int) error {
+	var err error
+	if len(nodes) == 0 {
+		nodes, err = d.NodeListInCluster(name)
+		if err != nil {
+			return fmt.Errorf("could not get IP list\n%s", err)
+		}
+	}
 	zones, err := d.getInstanceDetails(name, nodes)
 	if err != nil {
 		return err
@@ -695,6 +721,12 @@ func (d *backendGcp) TemplateDestroy(v backendVersion) error {
 
 func (d *backendGcp) CopyFilesToCluster(name string, files []fileList, nodes []int) error {
 	var err error
+	if len(nodes) == 0 {
+		nodes, err = d.NodeListInCluster(name)
+		if err != nil {
+			return fmt.Errorf("could not get IP list\n%s", err)
+		}
+	}
 	nodeIps, err := d.GetNodeIpMap(name, false)
 	if err != nil {
 		return fmt.Errorf("could not get node ip map for cluster: %s", err)
@@ -1162,14 +1194,6 @@ func (d *backendGcp) DeployTemplate(v backendVersion, script string, files []fil
 				Items: []*computepb.Items{
 					{
 						Key:   proto.String("ssh-keys"),
-						Value: proto.String("rglonek:" + string(sshKey)),
-					},
-					{
-						Key:   proto.String("ssh-keys"),
-						Value: proto.String("user:" + string(sshKey)),
-					},
-					{
-						Key:   proto.String("ssh-keys"),
 						Value: proto.String("root:" + string(sshKey)),
 					},
 				},
@@ -1227,76 +1251,35 @@ func (d *backendGcp) DeployTemplate(v backendVersion, script string, files []fil
 		return fmt.Errorf("unable to wait for the operation: %w", err)
 	}
 
-	// TODO: wait for instances to be made available via SSH
-	instanceCount := 1
-	instanceReady := 0
-	timeStart := time.Now()
-	var nout *ec2.DescribeInstancesOutput
-	for instanceReady < instanceCount {
-		for _, reservation := range reservations {
-			for _, instance := range reservation.Instances {
-				err := d.ec2svc.WaitUntilInstanceRunning(&ec2.DescribeInstancesInput{
-					InstanceIds: []*string{instance.InstanceId},
-				})
-				if err != nil {
-					return fmt.Errorf("failed on waiting for instance to be running: %s", err)
-				}
-				// get PublicIpAddress as it's not assigned at first so the field is empty
-				for ntry := 1; ntry <= 3; ntry += 1 {
-					nout, err = d.ec2svc.DescribeInstances(&ec2.DescribeInstancesInput{
-						InstanceIds: []*string{instance.InstanceId},
-					})
-					if err != nil {
-						if strings.Contains(err.Error(), "Request limit exceeded") && ntry < 3 {
-							time.Sleep(time.Duration(ntry*10) * time.Second)
-						} else {
-							return fmt.Errorf("failed on describe instance which is running: %s", err)
-						}
-					} else {
-						break
-					}
-				}
-
-				if len(nout.Reservations) == 0 {
-					fmt.Println("Got zero reservations back")
-					time.Sleep(time.Second)
-					continue
-				}
-
-				if len(nout.Reservations[0].Instances) == 0 {
-					fmt.Println("AWS returned 0 instances running")
-					time.Sleep(time.Second)
-					continue
-				}
-
-				if nout.Reservations[0].Instances[0].PublicIpAddress == nil {
-					fmt.Println("Have not received Public IP Address from AWS - just slow, or subnet in AWS is misconfigured - must be default provide public IP address")
-					time.Sleep(time.Second)
-					continue
-				}
-				if len(deployAwsTemplateShutdownMaking) > 0 {
-					for {
-						time.Sleep(time.Second)
-					}
-				}
-
-				_, err = remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *nout.Reservations[0].Instances[0].PublicIpAddress), keyPath, "ls", 0)
-				if err == nil {
-					instanceReady = instanceReady + 1
-				} else {
-					fmt.Printf("Not up yet, waiting (%s:22 using %s): %s\n", *nout.Reservations[0].Instances[0].PublicIpAddress, keyPath, err)
-					time.Sleep(time.Second)
-				}
-			}
+	inst, err := instancesClient.Get(ctx, &computepb.GetInstanceRequest{
+		Project:  a.opts.Config.Backend.Project,
+		Zone:     extra.zone,
+		Instance: name,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to poll new instance: %s", err)
+	}
+	if len(inst.NetworkInterfaces) < 1 || len(inst.NetworkInterfaces[0].AccessConfigs) < 1 || inst.NetworkInterfaces[0].AccessConfigs[0].NatIP == nil {
+		return errors.New("instance failed to obtain public IP")
+	}
+	instIp := *inst.NetworkInterfaces[0].AccessConfigs[0].NatIP
+	if len(deployGcpTemplateShutdownMaking) > 0 {
+		for {
+			time.Sleep(time.Second)
 		}
-		td := time.Since(timeStart)
-		if td.Seconds() > 600 {
-			return errors.New("waited for 600 seconds for SSH to come up, but it hasn't. Giving up")
+	}
+	for {
+		// wait for instances to be made available via SSH
+		_, err = remoteRun("root", fmt.Sprintf("%s:22", instIp), keyPath, "ls", 0)
+		if err == nil {
+			break
+		} else {
+			fmt.Printf("Not up yet, waiting (%s:22 using %s): %s\n", instIp, keyPath, err)
+			time.Sleep(time.Second)
 		}
 	}
 
-	instance := nout.Reservations[0].Instances[0]
-	if len(deployAwsTemplateShutdownMaking) > 0 {
+	if len(deployGcpTemplateShutdownMaking) > 0 {
 		for {
 			time.Sleep(time.Second)
 		}
@@ -1304,134 +1287,166 @@ func (d *backendGcp) DeployTemplate(v backendVersion, script string, files []fil
 
 	fmt.Println("Connection succeeded, continuing deployment...")
 
-	// sort out root/ubuntu issues
-	_, err = remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo mkdir -p /root/.ssh", 0)
-	if err != nil {
-		out, err := remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo mkdir -p /root/.ssh", 0)
+	/*
+		// sort out root/ubuntu issues
+		_, err = remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo mkdir -p /root/.ssh", 0)
 		if err != nil {
-			return fmt.Errorf("mkdir .ssh failed: %s\n%s", string(out), err)
+			out, err := remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo mkdir -p /root/.ssh", 0)
+			if err != nil {
+				return fmt.Errorf("mkdir .ssh failed: %s\n%s", string(out), err)
+			}
 		}
-	}
-	_, err = remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo chown root:root /root/.ssh", 0)
-	if err != nil {
-		out, err := remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo chown root:root /root/.ssh", 0)
+		_, err = remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo chown root:root /root/.ssh", 0)
 		if err != nil {
-			return fmt.Errorf("chown .ssh failed: %s\n%s", string(out), err)
+			out, err := remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo chown root:root /root/.ssh", 0)
+			if err != nil {
+				return fmt.Errorf("chown .ssh failed: %s\n%s", string(out), err)
+			}
 		}
-	}
-	_, err = remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo chmod 750 /root/.ssh", 0)
-	if err != nil {
-		out, err := remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo chmod 750 /root/.ssh", 0)
+		_, err = remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo chmod 750 /root/.ssh", 0)
 		if err != nil {
-			return fmt.Errorf("chmod .ssh failed: %s\n%s", string(out), err)
+			out, err := remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo chmod 750 /root/.ssh", 0)
+			if err != nil {
+				return fmt.Errorf("chmod .ssh failed: %s\n%s", string(out), err)
+			}
 		}
-	}
-	_, err = remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo cp /home/"+d.getUser(v)+"/.ssh/authorized_keys /root/.ssh/", 0)
-	if err != nil {
-		out, err := remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo cp /home/"+d.getUser(v)+"/.ssh/authorized_keys /root/.ssh/", 0)
+		_, err = remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo cp /home/"+d.getUser(v)+"/.ssh/authorized_keys /root/.ssh/", 0)
 		if err != nil {
-			return fmt.Errorf("cp .ssh failed: %s\n%s", string(out), err)
+			out, err := remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo cp /home/"+d.getUser(v)+"/.ssh/authorized_keys /root/.ssh/", 0)
+			if err != nil {
+				return fmt.Errorf("cp .ssh failed: %s\n%s", string(out), err)
+			}
 		}
-	}
-	_, err = remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo chmod 640 /root/.ssh/authorized_keys", 0)
-	if err != nil {
-		out, err := remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo chmod 640 /root/.ssh/authorized_keys", 0)
+		_, err = remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo chmod 640 /root/.ssh/authorized_keys", 0)
 		if err != nil {
-			return fmt.Errorf("chmod auth_keys failed: %s\n%s", string(out), err)
+			out, err := remoteRun(d.getUser(v), fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "sudo chmod 640 /root/.ssh/authorized_keys", 0)
+			if err != nil {
+				return fmt.Errorf("chmod auth_keys failed: %s\n%s", string(out), err)
+			}
 		}
-	}
-	if len(deployAwsTemplateShutdownMaking) > 0 {
-		for {
-			time.Sleep(time.Second)
+		if len(deployAwsTemplateShutdownMaking) > 0 {
+			for {
+				time.Sleep(time.Second)
+			}
 		}
-	}
+	*/
 
 	// copy files as required to VM
 	files = append(files, fileList{"/root/installer.sh", strings.NewReader(script), len(script)})
-	err = scp("root", fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, files)
+	err = scp("root", fmt.Sprintf("%s:22", instIp), keyPath, files)
 	if err != nil {
 		return fmt.Errorf("scp failed: %s", err)
 	}
-	if len(deployAwsTemplateShutdownMaking) > 0 {
+	if len(deployGcpTemplateShutdownMaking) > 0 {
 		for {
 			time.Sleep(time.Second)
 		}
 	}
 
 	// run script as required
-	_, err = remoteRun("root", fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "chmod 755 /root/installer.sh", 0)
+	_, err = remoteRun("root", fmt.Sprintf("%s:22", instIp), keyPath, "chmod 755 /root/installer.sh", 0)
 	if err != nil {
-		out, err := remoteRun("root", fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "chmod 755 /root/installer.sh", 0)
+		out, err := remoteRun("root", fmt.Sprintf("%s:22", instIp), keyPath, "chmod 755 /root/installer.sh", 0)
 		if err != nil {
 			return fmt.Errorf("chmod failed: %s\n%s", string(out), err)
 		}
 	}
-	out, err := remoteRun("root", fmt.Sprintf("%s:22", *instance.PublicIpAddress), keyPath, "/bin/bash -c /root/installer.sh", 0)
+	out, err := remoteRun("root", fmt.Sprintf("%s:22", instIp), keyPath, "/bin/bash -c /root/installer.sh", 0)
 	if err != nil {
 		return fmt.Errorf("/root/installer.sh failed: %s\n%s", string(out), err)
 	}
-	if len(deployAwsTemplateShutdownMaking) > 0 {
+	if len(deployGcpTemplateShutdownMaking) > 0 {
 		for {
 			time.Sleep(time.Second)
 		}
 	}
 
 	// shutdown VM
-	input1 := &ec2.StopInstancesInput{
-		InstanceIds: []*string{
-			aws.String(*instance.InstanceId),
-		},
-		DryRun: aws.Bool(false),
+	reqStop := &computepb.StopInstanceRequest{
+		Project:  a.opts.Config.Backend.Project,
+		Zone:     *inst.Zone,
+		Instance: *inst.Name,
 	}
-	result, err := d.ec2svc.StopInstances(input1)
+	op, err = instancesClient.Stop(ctx, reqStop)
 	if err != nil {
-		return fmt.Errorf("error stopping instance %s\n%s\n%s", *instance.InstanceId, result, err)
+		return fmt.Errorf("unable to start instance: %w", err)
 	}
-	d.ec2svc.WaitUntilInstanceStopped(&ec2.DescribeInstancesInput{
-		InstanceIds: []*string{instance.InstanceId},
-	})
-	if len(deployAwsTemplateShutdownMaking) > 0 {
+	if err = op.Wait(ctx); err != nil {
+		return fmt.Errorf("unable to wait for the operation: %w", err)
+	}
+
+	if len(deployGcpTemplateShutdownMaking) > 0 {
 		for {
 			time.Sleep(time.Second)
 		}
 	}
 
 	// create ami from VM
-	cii := ec2.CreateImageInput{}
-	cii.InstanceId = instance.InstanceId
-	cii.DryRun = aws.Bool(false)
-	cii.Name = aws.String(fmt.Sprintf("aerolab4-template-%s_%s_%s_%s", v.distroName, v.distroVersion, v.aerospikeVersion, isArm))
-	cii.NoReboot = aws.Bool(true)
-	cio, err := d.ec2svc.CreateImage(&cii)
+	imgName := fmt.Sprintf("aerolab4-template-%s_%s_%s_%s", v.distroName, v.distroVersion, v.aerospikeVersion, isArm)
+	err = d.makeImageFromDisk(*inst.Zone, *inst.Disks[0].DeviceName, imgName, labels)
 	if err != nil {
-		return fmt.Errorf("error creating AMI from instance %s", err)
+		return fmt.Errorf("makeImageFromDisk: %s", err)
 	}
-	imageId := cio.ImageId
-	d.ec2svc.WaitUntilImageAvailable(&ec2.DescribeImagesInput{
-		DryRun:   aws.Bool(false),
-		ImageIds: []*string{imageId},
-	})
-	// add tags to the image
-	imageTags := []*ec2.Tag{&tgClusterName, &tgNodeNumber, &tgNodeNumber1, &tgUsedBy, &tgName, &tgArch}
-	imageTags = append(imageTags, extraTags...)
-	cti := ec2.CreateTagsInput{
-		DryRun:    aws.Bool(false),
-		Resources: []*string{imageId},
-		Tags:      imageTags,
-	}
-	d.ec2svc.CreateTags(&cti)
 
-	// erase VM
-	input2 := &ec2.TerminateInstancesInput{
-		InstanceIds: []*string{
-			aws.String(*instance.InstanceId),
-		},
-		DryRun: aws.Bool(false),
+	// destroy VM
+	reqd := &computepb.DeleteInstanceRequest{
+		Project:  a.opts.Config.Backend.Project,
+		Zone:     *inst.Zone,
+		Instance: *inst.Name,
 	}
-	result1, err := d.ec2svc.TerminateInstances(input2)
+	op, err = instancesClient.Delete(ctx, reqd)
 	if err != nil {
-		return fmt.Errorf("error terminating template instance %s\n%s\n%s", *instance.InstanceId, result1, err)
+		return fmt.Errorf("unable to destroy instance: %w", err)
 	}
+	//if err = op.Wait(ctx); err != nil {
+	//	return fmt.Errorf("unable to wait for the operation: %w", err)
+	//}
+	return nil
+}
+
+func (d *backendGcp) makeImageFromDisk(zone string, sourceDiskName string, imageName string, imageLabels map[string]string) error {
+	ctx := context.Background()
+	disksClient, err := compute.NewDisksRESTClient(ctx)
+	if err != nil {
+		return fmt.Errorf("NewDisksRESTClient: %w", err)
+	}
+	defer disksClient.Close()
+	imagesClient, err := compute.NewImagesRESTClient(ctx)
+	if err != nil {
+		return fmt.Errorf("NewImagesRESTClient: %w", err)
+	}
+	defer imagesClient.Close()
+
+	// Get the source disk
+	source_req := &computepb.GetDiskRequest{
+		Disk:    sourceDiskName,
+		Project: a.opts.Config.Backend.Project,
+		Zone:    zone,
+	}
+
+	disk, err := disksClient.Get(ctx, source_req)
+	if err != nil {
+		return fmt.Errorf("unable to get source disk: %w", err)
+	}
+
+	// Create the image
+	req := computepb.InsertImageRequest{
+		ForceCreate: proto.Bool(true),
+		ImageResource: &computepb.Image{
+			Name:             &imageName,
+			SourceDisk:       disk.SelfLink,
+			StorageLocations: nil,
+			Labels:           imageLabels,
+		},
+		Project: a.opts.Config.Backend.Project,
+	}
+
+	op, err := imagesClient.Insert(ctx, &req)
+
+	if err = op.Wait(ctx); err != nil {
+		return fmt.Errorf("unable to wait for the operation: %w", err)
+	}
+
 	return nil
 }
 
