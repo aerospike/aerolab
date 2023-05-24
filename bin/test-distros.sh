624ad209f0daa36fe0fa20bc11c6fca0a33a0828
Robert Glonek <rglonek@aerospike.com>
2023-05-24 16:40:58 -0700

    update tests

--- bin/test-distros.sh
+++ bin/test-distros.sh
@@ -9,7 +9,13 @@ comm="aerolab-next"
 project="aerolab-test-project-1"
 region="ca-central-1"
 
+set -e
+export AEROLAB_CONFIG_FILE=deleteme.conf
 $comm config backend -t $1 -o ${project} -r ${region}
+$comm config defaults -k '*.SshKeyPath' -v /Users/rglonek/aerolab/keys
+$comm config defaults -k '*.FeaturesFilePath' -v /Users/rglonek/aerolab/features
+$comm config defaults -k '*.ChDir' -v /Users/rglonek/aerolab/chdir
+set +e
 if [ "$1" = "aws" ]
 then
     $comm config aws create-security-groups
@@ -26,14 +32,14 @@ $comm template destroy -d all -i all -v all
 $comm template vacuum
 
 set -e
-$comm cluster create -v 6.3.0.3 -d ubuntu -i 22.04 --instance-type t3a.medium --instance e2-medium
-$comm cluster grow -v 6.3.0.3 -d ubuntu -i 20.04 --instance-type t3a.medium --instance e2-medium
-$comm cluster grow -v 6.3.0.3 -d debian -i 10 --instance-type t3a.medium --instance e2-medium
-$comm cluster grow -v 6.3.0.3 -d debian -i 11 --instance-type t3a.medium --instance e2-medium
-$comm cluster grow -v 6.3.0.3 -d centos -i 7 --instance-type t3a.medium --instance e2-medium
-$comm cluster grow -v 6.3.0.3 -d centos -i 8 --instance-type t3a.medium --instance e2-medium
-$comm cluster grow -v 6.3.0.3 -d centos -i 9 --instance-type t3a.medium --instance e2-medium
-$comm cluster grow -v 6.3.0.3 -d amazon -i 2 --instance-type t3a.medium --instance e2-medium
+$comm cluster create -v 6.3.0.3 -d ubuntu -i 22.04 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm cluster grow -v 6.3.0.3 -d ubuntu -i 20.04 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm cluster grow -v 6.3.0.3 -d debian -i 10 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm cluster grow -v 6.3.0.3 -d debian -i 11 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm cluster grow -v 6.3.0.3 -d centos -i 7 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm cluster grow -v 6.3.0.3 -d centos -i 8 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm cluster grow -v 6.3.0.3 -d centos -i 9 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm cluster grow -v 6.3.0.3 -d amazon -i 2 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
 set +e
 
 $comm cluster destroy -f
@@ -41,14 +47,16 @@ $comm template destroy -d all -i all -v all
 $comm template vacuum
 
 set -e
-$comm client create base -d ubuntu -i 22.04 --instance-type t3a.medium --instance e2-medium
-$comm client grow base -d ubuntu -i 20.04 --instance-type t3a.medium --instance e2-medium
-$comm client grow base -d debian -i 10 --instance-type t3a.medium --instance e2-medium
-$comm client grow base -d debian -i 11 --instance-type t3a.medium --instance e2-medium
-$comm client grow base -d centos -i 7 --instance-type t3a.medium --instance e2-medium
-$comm client grow base -d centos -i 8 --instance-type t3a.medium --instance e2-medium
-$comm client grow base -d centos -i 9 --instance-type t3a.medium --instance e2-medium
-$comm client grow base -d amazon -i 2 --instance-type t3a.medium --instance e2-medium
+$comm client create base -d ubuntu -i 22.04 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm client grow base -d ubuntu -i 20.04 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm client grow base -d debian -i 10 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm client grow base -d debian -i 11 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm client grow base -d centos -i 7 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm client grow base -d centos -i 8 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm client grow base -d centos -i 9 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
+$comm client grow base -d amazon -i 2 --instance-type t3a.medium --instance e2-medium --zone us-central1-a
 set +e
 
 $comm client destroy -f -n client
+
+rm -f deleteme.conf
