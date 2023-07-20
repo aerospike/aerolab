77639617e1540d90c6ffe787e5167a575b5ad88f
Robert Glonek <rglonek@aerospike.com>
2023-07-20 15:48:21 -0700

    remove checksum making

--- bin/notarize/sign-notarize-all.sh
+++ bin/notarize/sign-notarize-all.sh
@@ -2,4 +2,4 @@
 sleep 60
 ./sign-zip-notarize.sh || exit 1
 cd ../packages
-rm -f sha256.sum; ls |while read line; do shasum -a 256 $line >> sha256.sum; done
+#rm -f sha256.sum; ls |while read line; do shasum -a 256 $line >> sha256.sum; done
