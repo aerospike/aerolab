fe09ae3658066204613272cc34163b8521ff318d
Robert Glonek <rglonek@aerospike.com>
2023-08-10 13:48:38 -0700

    gcp expiries function embedding

--- src/embedForCloudExpiries.go
+++ src/embedForCloudExpiries.go
@@ -4,5 +4,14 @@ import (
 	_ "embed"
 )
 
+//go:generate sh -c "cat ../expiries/gcp/go.mod > gcpMod.txt"
+//go:generate sh -c "cat ../expiries/gcp/function.go > gcpFunction.txt"
+
 //go:embed myFunction.zip
 var expiriesCodeAws []byte
+
+//go:embed gcpMod.txt
+var expiriesCodeGcpMod []byte
+
+//go:embed gcpFunction.txt
+var expiriesCodeGcpFunction []byte
