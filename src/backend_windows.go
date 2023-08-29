c2f27021ed90415f005856e2006c376f20db31ca
Robert Glonek <rglonek@aerospike.com>
2023-08-29 15:05:15 -0700

    make ssh work on windows

---
+++ src/backend_windows.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"io"
+	"os"
+
+	"github.com/containerd/console"
+	"golang.org/x/crypto/ssh"
+	"golang.org/x/term"
+)
+
+func (ssh_client *SSH) RunAttachCmd(cmd string, stdin io.Reader, stdout io.Writer, stderr io.Writer) error {
+	ssh_client.session.Stdin = stdin
+	ssh_client.session.Stdout = stdout
+	ssh_client.session.Stderr = stderr
+	fileDescriptor := int(os.Stdout.Fd())
+	var err error
+	termWidth := 80
+	termHeight := 24
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          1,     // enable echoing
+		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
+		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
+	}
+	if term.IsTerminal(fileDescriptor) {
+		termWidth, termHeight, err = term.GetSize(fileDescriptor)
+		if err != nil {
+			return err
+		}
+
+		err = ssh_client.session.RequestPty("vt100", termHeight, termWidth, modes)
+		if err != nil {
+			return err
+		}
+	} else {
+		err = ssh_client.session.RequestPty("vt100", 24, 80, modes)
+		if err != nil {
+			return err
+		}
+	}
+	current := console.Current()
+	defer current.Reset()
+
+	if err := current.SetRaw(); err != nil {
+	}
+	ws, err := current.Size()
+	current.Resize(ws)
+	err = ssh_client.session.Run(cmd)
+	return err
+}
