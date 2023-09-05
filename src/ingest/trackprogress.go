59f6174168a71b6f9fcbab8af61d50e7a48ef61c
Robert Glonek <rglonek@aerospike.com>
2023-09-05 12:20:40 -0700

    ingest - downloader

--- src/ingest/trackprogress.go
+++ src/ingest/trackprogress.go
@@ -3,35 +3,49 @@ package ingest
 import (
 	"compress/gzip"
 	"encoding/json"
-	"log"
 	"os"
 	"path"
 	"time"
+
+	"github.com/bestmethod/logger"
 )
 
 func (i *Ingest) loadProgress() error {
+	i.progress.Downloader.S3Files = make(map[string]*downloaderS3File)
 	dir, _ := path.Split(i.config.ProgressFile.OutputFilePath)
 	os.MkdirAll(dir, 0755)
 	if _, err := os.Stat(i.config.ProgressFile.OutputFilePath); os.IsNotExist(err) {
+		logger.Debug("INIT: Not loading progress - file not found")
 		return nil
 	}
+	logger.Debug("INIT: Loading progress")
 	f, err := os.Open(i.config.ProgressFile.OutputFilePath)
 	if err != nil {
 		return err
 	}
 	defer f.Close()
-	return json.NewDecoder(f).Decode(i.progress)
+	if !i.config.ProgressFile.Compress {
+		return json.NewDecoder(f).Decode(i.progress)
+	}
+	fgz, err := gzip.NewReader(f)
+	if err != nil {
+		return err
+	}
+	defer fgz.Close()
+	return json.NewDecoder(fgz).Decode(i.progress)
 }
 
 func (i *Ingest) saveProgressInterval() {
 	if i.config.ProgressFile.DisableWrite {
+		logger.Debug("INIT: saving progress is disabled")
 		return
 	}
+	logger.Debug("INIT: saving progress will run every %v", i.config.ProgressFile.WriteInterval)
 	for {
 		time.Sleep(i.config.ProgressFile.WriteInterval)
 		err := i.saveProgress()
 		if err != nil {
-			log.Printf("WARN: progress could not be saved: %s", err)
+			logger.Warn("Progress could not be saved: %s", err)
 		}
 	}
 }
@@ -40,12 +54,15 @@ func (i *Ingest) saveProgress() error {
 	i.progress.Lock()
 	defer i.progress.Unlock()
 	if !i.progress.changed {
+		logger.Detail("SAVE-PROGRESS Not changed, not saving")
 		return nil
 	}
+	logger.Detail("SAVE-PROGRESS Saving")
 	err := i.saveProgressDo()
 	if err != nil {
 		return err
 	}
+	logger.Detail("SAVE-PROGRESS Saved, rename and return")
 	return os.Rename(i.config.ProgressFile.OutputFilePath+".tmp", i.config.ProgressFile.OutputFilePath)
 }
 func (i *Ingest) saveProgressDo() error {
@@ -57,6 +74,7 @@ func (i *Ingest) saveProgressDo() error {
 	var enc *json.Encoder
 	if i.config.ProgressFile.Compress {
 		fgz := gzip.NewWriter(f)
+		defer fgz.Close()
 		enc = json.NewEncoder(fgz)
 	} else {
 		enc = json.NewEncoder(f)
@@ -71,20 +89,22 @@ func (i *Ingest) saveProgressDo() error {
 
 func (i *Ingest) printProgressInterval() {
 	if !i.config.ProgressPrint.Enable {
+		logger.Debug("PRINT-PROGRESS Not enabled, will not print")
 		return
 	}
+	logger.Debug("PRINT-PROGRESS Will print every %v", i.config.ProgressPrint.UpdateInterval)
 	for {
 		time.Sleep(i.config.ProgressPrint.UpdateInterval)
 		err := i.printProgress()
 		if err != nil {
-			log.Printf("WARN: progress could not be printed: %s", err)
+			logger.Warn("Progress could not be printed: %s", err)
 		}
 	}
 }
 
 func (i *Ingest) printProgress() error {
-	i.progress.Lock()
-	defer i.progress.Unlock()
+	i.progress.RLock()
+	defer i.progress.RUnlock()
 	enc := json.NewEncoder(os.Stdout)
 	enc.SetIndent("", "  ")
 	return enc.Encode(i.progress)
