e851e7871e5d88c83b1ba91479353bb73521c5a2
Robert Glonek <rglonek@aerospike.com>
2023-09-06 11:27:09 -0700

    ingest pre-processor todo

--- src/ingest/preprocess.go
+++ src/ingest/preprocess.go
@@ -1,1 +1,10 @@
 package ingest
+
+/*
+TODO
+* for each file
+  * if collectinfo, move to collectinfo dir
+  * if aerospike log file, work out cluster name, node id, and move to logs/ dir
+* move whatever is left to other dir
+* now rename collectinfos (in the background?) as we now know what is what
+*/
