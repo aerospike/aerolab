f254cd2b642ec246dd35abe817870264fb99fadb
Robert Glonek <rglonek@aerospike.com>
2023-09-14 12:12:17 -0700

    plugin metrics and variables query

--- src/plugin/frontendHandleTagKeys.go
+++ src/plugin/frontendHandleTagKeys.go
@@ -2,4 +2,7 @@ package plugin
 
 import "net/http"
 
-func (p *Plugin) handleTagKeys(w http.ResponseWriter, r *http.Request) {}
+func (p *Plugin) handleTagKeys(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotImplemented)
+	w.Write([]byte("not implemented"))
+}
