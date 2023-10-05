ac9392a61348e78ba74edac2c2467811dd2952b7
Robert Glonek <rglonek@aerospike.com>
2023-10-05 13:11:17 -0700

    partition configurator basic asd7 support

--- src/cmdClusterPartitionConf.go
+++ src/cmdClusterPartitionConf.go
@@ -22,7 +22,7 @@ type clusterPartitionConfCmd struct {
 	FilterPartitions   TypeFilterRange `short:"p" long:"filter-partitions" description:"Select partitions on each disk by number; 0=use entire disk itself, ex: 1,2,4-8" default:"ALL"`
 	FilterType         string          `short:"t" long:"filter-type" description:"what disk types to select, options: nvme/local or ebs/persistent" default:"ALL"`
 	Namespace          string          `short:"m" long:"namespace" description:"namespace to modify the settings for" default:""`
-	ConfDest           string          `short:"o" long:"configure" description:"what to configure the selections as; options: device|shadow|pi-flash|si-flash" default:""`
+	ConfDest           string          `short:"o" long:"configure" description:"what to configure the selections as; options: memory|device|shadow|pi-flash|si-flash" default:""`
 	MountsSizeLimitPct float64         `short:"s" long:"mounts-size-limit-pct" description:"specify %% for what the mounts-size-limit should be set to for flash configs" default:"90"`
 	parallelThreadsLongCmd
 	Help helpCmd `command:"help" subcommands-optional:"true" description:"Print help"`
@@ -40,8 +40,8 @@ func (c *clusterPartitionConfCmd) Execute(args []string) error {
 	} else if c.FilterType == "persistent" {
 		c.FilterType = "ebs"
 	}
-	if !inslice.HasString([]string{"device", "shadow", "pi-flash", "si-flash", "allflash"}, c.ConfDest) {
-		return fmt.Errorf("configure options must be one of: device, shadow, pi-flash, si-flash, allflash")
+	if !inslice.HasString([]string{"memory", "device", "shadow", "pi-flash", "si-flash", "allflash"}, c.ConfDest) {
+		return fmt.Errorf("configure options must be one of: memory, device, shadow, pi-flash, si-flash, allflash")
 	}
 	if c.Namespace == "" {
 		return fmt.Errorf("namespace name is required")
@@ -110,7 +110,22 @@ func (c *clusterPartitionConfCmd) doParallel(nodeNo int, disks map[int]map[int]b
 }
 
 func (c *clusterPartitionConfCmd) do(nodeNo int, disks map[int]map[int]blockDevices, filterDiskCount int, filterPartCount int) error {
-	outn, err := b.RunCommands(c.ClusterName.String(), [][]string{{"cat", "/etc/aerospike/aerospike.conf"}}, []int{nodeNo})
+	is7 := "mounts-budget"
+	outn, err := b.RunCommands(c.ClusterName.String(), [][]string{{"cat", "/opt/aerolab.aerospike.version"}}, []int{nodeNo})
+	if err != nil {
+		log.Printf("could not read /opt/aerolab.aerospike.version on node %d, assuming asd version 7: %s", nodeNo, err)
+	} else {
+		major := strings.Trim(strings.Split(string(outn[0]), ".")[0], "\r\n\t ")
+		majorInt, err := strconv.Atoi(major)
+		if err != nil {
+			log.Printf("could not parse /opt/aerolab.aerospike.version on node %d, assuming asd version 7: %s", nodeNo, err)
+		} else {
+			if majorInt < 7 {
+				is7 = "mounts-size-limit"
+			}
+		}
+	}
+	outn, err = b.RunCommands(c.ClusterName.String(), [][]string{{"cat", "/etc/aerospike/aerospike.conf"}}, []int{nodeNo})
 	if err != nil {
 		return fmt.Errorf("could not read aerospike.conf on node %d: %s", nodeNo, err)
 	}
@@ -123,6 +138,19 @@ func (c *clusterPartitionConfCmd) do(nodeNo int, disks map[int]map[int]blockDevi
 		cc.NewStanza("namespace " + c.Namespace)
 	}
 	switch c.ConfDest {
+	case "memory":
+		for _, key := range cc.Stanza("namespace " + c.Namespace).ListKeys() {
+			if strings.HasPrefix(key, "storage-engine") && (!strings.HasSuffix(key, "memory") || cc.Stanza("namespace "+c.Namespace).Type("storage-engine memory") != aeroconf.ValueStanza) {
+				cc.Stanza("namespace " + c.Namespace).Delete(key)
+			}
+		}
+		if cc.Stanza("namespace "+c.Namespace).Type("storage-engine memory") == aeroconf.ValueNil {
+			cc.Stanza("namespace " + c.Namespace).NewStanza("storage-engine memory")
+		}
+		cc.Stanza("namespace " + c.Namespace).Stanza("storage-engine memory").Delete("device")
+		cc.Stanza("namespace " + c.Namespace).Stanza("storage-engine memory").Delete("file")
+		cc.Stanza("namespace " + c.Namespace).Stanza("storage-engine memory").Delete("filesize")
+		cc.Stanza("namespace " + c.Namespace).Stanza("storage-engine memory").Delete("data-size")
 	case "device":
 		for _, key := range cc.Stanza("namespace " + c.Namespace).ListKeys() {
 			if strings.HasPrefix(key, "storage-engine") && (!strings.HasSuffix(key, "device") || cc.Stanza("namespace "+c.Namespace).Type("storage-engine device") != aeroconf.ValueStanza) {
@@ -136,10 +164,10 @@ func (c *clusterPartitionConfCmd) do(nodeNo int, disks map[int]map[int]blockDevi
 		cc.Stanza("namespace " + c.Namespace).Stanza("storage-engine device").Delete("file")
 		cc.Stanza("namespace " + c.Namespace).Stanza("storage-engine device").Delete("filesize")
 	case "shadow":
-		if cc.Stanza("namespace "+c.Namespace).Type("storage-engine device") != aeroconf.ValueStanza {
-			return fmt.Errorf("storage-engine device configuration not found for namespace")
+		if cc.Stanza("namespace "+c.Namespace).Type("storage-engine device") != aeroconf.ValueStanza && cc.Stanza("namespace "+c.Namespace).Type("storage-engine memory") != aeroconf.ValueStanza {
+			return fmt.Errorf("storage-engine device|memory configuration not found for namespace")
 		}
-		if !inslice.HasString(cc.Stanza("namespace "+c.Namespace).Stanza("storage-engine device").ListKeys(), "device") {
+		if !inslice.HasString(cc.Stanza("namespace "+c.Namespace).Stanza("storage-engine device").ListKeys(), "device") && !inslice.HasString(cc.Stanza("namespace "+c.Namespace).Stanza("storage-engine memory").ListKeys(), "device") {
 			return fmt.Errorf("no device configured for given namespace, cannot add shadow devices")
 		}
 	case "allflash":
@@ -205,6 +233,14 @@ func (c *clusterPartitionConfCmd) do(nodeNo int, disks map[int]map[int]blockDevi
 	})
 	for _, p := range useParts {
 		switch c.ConfDest {
+		case "memory":
+			vals, err := cc.Stanza("namespace " + c.Namespace).Stanza("storage-engine memory").GetValues("device")
+			if err != nil {
+				return err
+			}
+			pp := p.Path
+			vals = append(vals, &pp)
+			cc.Stanza("namespace "+c.Namespace).Stanza("storage-engine memory").SetValues("device", vals)
 		case "device":
 			vals, err := cc.Stanza("namespace " + c.Namespace).Stanza("storage-engine device").GetValues("device")
 			if err != nil {
@@ -214,7 +250,11 @@ func (c *clusterPartitionConfCmd) do(nodeNo int, disks map[int]map[int]blockDevi
 			vals = append(vals, &pp)
 			cc.Stanza("namespace "+c.Namespace).Stanza("storage-engine device").SetValues("device", vals)
 		case "shadow":
-			vals, err := cc.Stanza("namespace " + c.Namespace).Stanza("storage-engine device").GetValues("device")
+			ntype := "memory"
+			if cc.Stanza("namespace "+c.Namespace).Stanza("storage-engine device") != nil {
+				ntype = "device"
+			}
+			vals, err := cc.Stanza("namespace " + c.Namespace).Stanza("storage-engine " + ntype).GetValues("device")
 			if err != nil {
 				return err
 			}
@@ -231,7 +271,7 @@ func (c *clusterPartitionConfCmd) do(nodeNo int, disks map[int]map[int]blockDevi
 			if !found {
 				return errors.New("not enough primary devices for chosen shadow devices")
 			}
-			cc.Stanza("namespace "+c.Namespace).Stanza("storage-engine device").SetValues("device", vals)
+			cc.Stanza("namespace "+c.Namespace).Stanza("storage-engine "+ntype).SetValues("device", vals)
 		case "allflash":
 			fallthrough
 		case "pi-flash":
@@ -260,7 +300,7 @@ func (c *clusterPartitionConfCmd) do(nodeNo int, disks map[int]map[int]blockDevi
 				}
 				fsSize = strconv.Itoa(int(float64(fsSizeI)*c.MountsSizeLimitPct/100)) + suffix
 			}
-			cc.Stanza("namespace "+c.Namespace).Stanza("index-type flash").SetValue("mounts-size-limit", fsSize)
+			cc.Stanza("namespace "+c.Namespace).Stanza("index-type flash").SetValue(is7, fsSize)
 		case "si-flash":
 			vals, err := cc.Stanza("namespace " + c.Namespace).Stanza("sindex-type flash").GetValues("mount")
 			if err != nil {
@@ -287,11 +327,15 @@ func (c *clusterPartitionConfCmd) do(nodeNo int, disks map[int]map[int]blockDevi
 				}
 				fsSize = strconv.Itoa(int(float64(fsSizeI)*c.MountsSizeLimitPct/100)) + suffix
 			}
-			cc.Stanza("namespace "+c.Namespace).Stanza("sindex-type flash").SetValue("mounts-size-limit", fsSize)
+			cc.Stanza("namespace "+c.Namespace).Stanza("sindex-type flash").SetValue(is7, fsSize)
 		}
 	}
 	if c.ConfDest == "shadow" {
-		vals, err := cc.Stanza("namespace " + c.Namespace).Stanza("storage-engine device").GetValues("device")
+		ntype := "memory"
+		if cc.Stanza("namespace "+c.Namespace).Stanza("storage-engine device") != nil {
+			ntype = "device"
+		}
+		vals, err := cc.Stanza("namespace " + c.Namespace).Stanza("storage-engine " + ntype).GetValues("device")
 		if err != nil {
 			return err
 		}
