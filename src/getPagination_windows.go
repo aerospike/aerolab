819cc7f397a66a20f92daa7b7d61bca06fd52710
Robert Glonek <rglonek@aerospike.com>
2023-10-06 11:36:35 -0700

    fix paging

--- src/getPagination_windows.go
+++ src/getPagination_windows.go
@@ -6,14 +6,14 @@ import (
 	"strings"
 )
 
-func getPaginationCommand() (string, []string) {
+func getPagerCommand() (string, []string) {
 	less, err := exec.Command("cmd", "/c", "WHERE less").CombinedOutput()
 	if err == nil {
-		return strings.Trim(string(less), "\r\n\t "), []string{"-S", "-r"}
+		return strings.Trim(string(less), "\r\n\t "), []string{"-S", "-R"}
 	}
 	gnuWin := "C:\\Program Files (x86)\\GnuWin32\\bin\\less.exe"
 	if _, err := os.Stat(gnuWin); err == nil {
-		return strings.Trim(string(gnuWin), "\r\n\t "), []string{"-S", "-r"}
+		return strings.Trim(string(gnuWin), "\r\n\t "), []string{"-S", "-R"}
 	}
 	less, err = exec.Command("cmd", "/c", "WHERE more").CombinedOutput()
 	if err == nil {
