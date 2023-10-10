194cec0cc8922bb0c80314582299a4483252250f
Robert Glonek <rglonek@aerospike.com>
2023-10-10 13:22:58 -0700

    agi-create and agi-add-auth-token

--- src/cmdAgiCreate.script.cloud.sh
+++ src/cmdAgiCreate.script.cloud.sh
@@ -1,6 +1,7 @@
 set -e
 mkdir -p /opt/agi/aerospike/data
 apt update && apt -y install wget adduser libfontconfig1 musl && wget https://dl.grafana.com/oss/release/grafana_10.1.2_%s.deb && dpkg -i grafana_10.1.2_%s.deb
+chmod 755 /usr/local/bin/aerolab
 aerolab config backend -t none
 cat <<'EOF' > /etc/aerospike/aerospike.conf
 service {
@@ -103,10 +104,17 @@ Restart=on-failure
 User=root
 RestartSec=10
 WorkingDirectory=/opt/agi
-ExecStart=/usr/local/bin/aerolab agi exec proxy -L "%s" -a %s %s
+ExecStart=/usr/local/bin/aerolab agi exec proxy -L "%s" -a token -l %d %s -C %s -K %s -m %s -M %s
 
 [Install]
 WantedBy=multi-user.target
+EOF
+cat <<'EOF' > /opt/agi/grafanafix.yaml
+dashboards:
+  fromDir: ""
+  loadEmbedded: true
+grafanaURL: "http://127.0.0.1:8850"
+annotationFile: "/opt/agi/annotations.json"
 EOF
 cat <<'EOF' > /opt/agi/plugin.yaml
 maxDataPointsReceived: %d
@@ -118,67 +126,6 @@ addNoneToLabels:
   - HistogramSize
   - HistogramCount
 %s
-EOF
-cat <<'EOF' > /opt/agi/ingest.yaml
-logLevel: %d
-aerospike:
-  waitForSindexes: true
-  maxPutThreads: %d
-%s
-preProcessor:
-  fileThreads: 6
-  unpackerFileThreads: 4
-processor:
-  maxConcurrentLogFiles: 6
-progressFile:
-  disableWrite: false
-  writeInterval: 10s
-  compress: true
-  outputFilePath: "/opt/agi/ingest"
-progressPrint:
-  enable: true
-  updateInterval: 10s
-  printOverallProgress: true
-  printDetailProgress: true
-patternsFile: "%s"
-ingestTimeRanges:
-  enabled: %t
-  %s
-  %s
-directories:
-  collectInfo: "/opt/agi/files/collectinfo"
-  logs: "/opt/agi/files/logs"
-  dirtyTemp: "/opt/agi/files/input"
-  noStatOut: "/opt/agi/files/no-stat"
-  otherFiles: "/opt/agi/files/other"
-customSourceName: "%s"
-downloader:
-  sftpSource:
-    enabled: %t
-    threads: %d
-    host: "%s"
-    port: %d
-    username: "%s"
-    password: "%s"
-    keyFile: ""
-    pathPrefix: "%s"
-    searchRegex: "%s"
-  s3Source:
-    enabled: %t
-    threads: %d
-    region: "%s"
-    bucketName: "%s"
-    keyID: "%s"
-    secretKey: "%s"
-    pathPrefix: "%s"
-    searchRegex: "%s"
-EOF
-cat <<'EOF' > /opt/agi/grafanafix.yaml
-dashboards:
-  fromDir: ""
-  loadEmbedded: true
-grafanaURL: "http://127.0.0.1:8850"
-annotationFile: "/opt/agi/annotations.json"
 EOF
 chmod 755 /etc/systemd/system/agi-plugin.service /etc/systemd/system/agi-proxy.service /etc/systemd/system/agi-grafanafix.service /etc/systemd/system/agi-ingest.service
 systemctl enable agi-plugin && systemctl enable agi-proxy && systemctl enable agi-grafanafix && systemctl enable agi-ingest && systemctl daemon-reload
