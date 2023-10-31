926c3c739abc17df3424e80368b8550947674eed
Robert Glonek <rglonek@aerospike.com>
2023-10-31 12:31:02 -0700

    add sort options

--- src/cmdVolume.go
+++ src/cmdVolume.go
@@ -30,10 +30,12 @@ func (c *volumeCmd) Execute(args []string) error {
 }
 
 type volumeListCmd struct {
-	Json  bool    `short:"j" long:"json" description:"Provide output in json format"`
-	Owner string  `long:"owner" description:"filter by owner tag/label"`
-	Pager bool    `long:"pager" description:"set to enable vertical and horizontal pager"`
-	Help  helpCmd `command:"help" subcommands-optional:"true" description:"Print help"`
+	SortBy     []string `long:"sort-by" description:"sort by field name; must match exact header name; can be specified multiple times; format: asc:name dsc:name ascnum:name dscnum:name"`
+	Json       bool     `short:"j" long:"json" description:"Provide output in json format"`
+	JsonPretty bool     `short:"p" long:"pretty" description:"Provide json output with line-feeds and indentations"`
+	Owner      string   `long:"owner" description:"filter by owner tag/label"`
+	Pager      bool     `long:"pager" description:"set to enable vertical and horizontal pager"`
+	Help       helpCmd  `command:"help" subcommands-optional:"true" description:"Print help"`
 }
 
 func (c *volumeListCmd) Execute(args []string) error {
@@ -43,6 +45,8 @@ func (c *volumeListCmd) Execute(args []string) error {
 	a.opts.Inventory.List.Json = c.Json
 	a.opts.Inventory.List.Owner = c.Owner
 	a.opts.Inventory.List.Pager = c.Pager
+	a.opts.Inventory.List.SortBy = c.SortBy
+	a.opts.Inventory.List.JsonPretty = c.JsonPretty
 	return a.opts.Inventory.List.run(false, false, false, false, false, inventoryShowVolumes)
 }
 
