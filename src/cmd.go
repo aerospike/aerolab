6378be883a6c1c39f6223447503b9ee54e1e089e
Robert Glonek <rglonek@aerospike.com>
2023-11-20 11:59:07 -0800

    add aerolab upgrade command

--- src/cmd.go
+++ src/cmd.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
+	"runtime"
+	"strings"
+	"time"
 )
 
 type commands struct {
@@ -30,6 +37,7 @@ type commands struct {
 	AGI          agiCmd          `command:"agi" subcommands-optional:"true" description:"Launch or manage AGI troubleshooting instances"`
 	Volume       volumeCmd       `command:"volume" subcommands-optional:"true" description:"Volume management (AWS EFS only)"`
 	ShowCommands showcommandsCmd `command:"showcommands" subcommands-optional:"true" description:"Install showsysinfo,showconf,showinterrupts on the current system"`
+	Upgrade      upgradeCmd      `command:"upgrade" subcommands-optional:"true" description:"Upgrade AeroLab binary"`
 	commandsDefaults
 }
 
@@ -57,6 +65,160 @@ func (c *showcommandsCmd) Execute(args []string) error {
 		if err != nil {
 			log.Printf("ERROR symlinking %s->%s : %s", cur, d, err)
 		}
+	}
+	log.Println("Done")
+	return nil
+}
+
+type upgradeCmd struct {
+	Edge   bool    `long:"edge" description:"Include pre-releases when discovering latest version"`
+	DryRun bool    `long:"dryrun" description:"Set to show the upgrade source URL and destination path, do not upgrade"`
+	Help   helpCmd `command:"help" subcommands-optional:"true" description:"Print help"`
+}
+
+func (c *upgradeCmd) Execute(args []string) error {
+	if earlyProcessNoBackend(args) {
+		return nil
+	}
+	log.Println("Checking latest version...")
+	v := &GitHubRelease{}
+	var pre *GitHubRelease
+	err := a.isLatestVersionQuery(v, "")
+	if err != nil {
+		return err
+	}
+	latestVersion := ""
+	if c.Edge {
+		pre, err = a.isLatestVersionQueryPrerelease()
+		if err != nil {
+			if err.Error() == "NOT FOUND" {
+				c.Edge = false
+			} else {
+				return err
+			}
+		}
+	}
+	if !c.Edge {
+		ved := strings.Trim(vEdition, "\r\n\t ")
+		if len(v.CommitHash) > 8 {
+			v.CommitHash = v.CommitHash[0:7]
+		}
+		if ved == "-unofficial" || ved == "-prerelease" || VersionCheck(v.CurrentVersion, v.LatestVersion) > 0 {
+			latestVersion = "v" + v.LatestVersion + "-" + v.CommitHash + "-stable"
+		} else {
+			log.Println("Already on latest stable")
+			return nil
+		}
+	} else {
+		if pre.LatestVersion != pre.CurrentVersion {
+			latestVersion = "v" + pre.LatestVersion + "-prerelease"
+			v = pre
+		} else {
+			log.Println("Already on latest edge")
+			return nil
+		}
+	}
+	cur, err := os.Executable()
+	if err != nil {
+		return fmt.Errorf("failed to get absolute path os self: %s", err)
+	}
+	for {
+		if st, err := os.Stat(cur); err != nil {
+			return fmt.Errorf("failed to stat self: %s", err)
+		} else {
+			if st.Mode()&os.ModeSymlink > 0 {
+				cur, err = filepath.EvalSymlinks(cur)
+				if err != nil {
+					return fmt.Errorf("error resolving symlink source to self: %s", err)
+				}
+			} else {
+				break
+			}
+		}
+	}
+	from := ""
+	fn := "aerolab-"
+	switch runtime.GOOS {
+	case "windows", "linux":
+		fn = fn + runtime.GOOS + "-"
+	case "darwin", "macos":
+		fn = fn + "macos-"
+	default:
+		return fmt.Errorf("operating system %s not supported", runtime.GOOS)
+	}
+	switch runtime.GOARCH {
+	case "x86_64", "amd64":
+		fn = fn + "amd64-"
+	case "arm64", "aarch64":
+		fn = fn + "arm64-"
+	default:
+		return fmt.Errorf("cpu architecture %s not supported", runtime.GOARCH)
+	}
+	fn = fn + strings.Split(v.LatestVersion, "-")[0] + ".zip"
+	for _, pre := range v.Assets {
+		if pre.ContentType != "application/zip" {
+			continue
+		}
+		if pre.FileName != fn {
+			continue
+		}
+		from = pre.DownloadUrl
+		break
+	}
+	if from == "" {
+		return fmt.Errorf("asset (%s) not found in releases page", fn)
+	}
+	log.Printf("Upgrading %s => %s", version, latestVersion)
+	if c.DryRun {
+		log.Printf("DryRun: %s => %s", from, cur)
+		return nil
+	}
+	dest, err := os.OpenFile(cur+"-upgrade", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0755)
+	if err != nil {
+		return fmt.Errorf("failed to create temporary file '%s': %s", cur+"-upgrade", err)
+	}
+	defer dest.Close()
+	client := &http.Client{}
+	client.Timeout = 10 * time.Minute
+	defer client.CloseIdleConnections()
+	req, err := http.NewRequest("GET", from, nil)
+	if err != nil {
+		return err
+	}
+	response, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+	if response.StatusCode != 200 {
+		body, _ := io.ReadAll(response.Body)
+		err = fmt.Errorf("GET '%s': exit code (%d), message: %s", from, response.StatusCode, string(body))
+		return err
+	}
+	buf, err := io.ReadAll(response.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read body: %s", err)
+	}
+	zipc, err := zip.NewReader(bytes.NewReader(buf), int64(len(buf)))
+	if err != nil {
+		return fmt.Errorf("failed to open body as zip file: %s", err)
+	}
+	f, err := zipc.Open("aerolab")
+	if err != nil {
+		return fmt.Errorf("failed to open file 'aerolab' inside zip: %s", err)
+	}
+	defer f.Close()
+	_, err = io.Copy(dest, f)
+	if err != nil {
+		return fmt.Errorf("failed to unzip file: %s", err)
+	}
+	err = dest.Sync()
+	if err != nil {
+		return fmt.Errorf("failed to sync temp file to storage: %s", err)
+	}
+	err = os.Rename(cur+"-upgrade", cur)
+	if err != nil {
+		return fmt.Errorf("failed to rename temp file '%s' to final destination '%s': %s", cur+"-upgrade", cur, err)
 	}
 	log.Println("Done")
 	return nil
--- src/isLatestVersion.go
+++ src/isLatestVersion.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -13,8 +14,17 @@ import (
 )
 
 type GitHubRelease struct {
-	LatestVersion  string `json:"tag_name"`
-	CurrentVersion string `json:"current_version"`
+	LatestVersion  string            `json:"tag_name"`
+	CurrentVersion string            `json:"current_version"`
+	Assets         []*ghReleaseAsset `json:"assets"`
+	Prerelease     bool              `json:"prerelease"`
+	CommitHash     string            `json:"target_commitish"`
+}
+
+type ghReleaseAsset struct {
+	DownloadUrl string `json:"browser_download_url"`
+	FileName    string `json:"name"`         // aerolab-{windows|macos|linux}-{arm64|amd64}-{version}.zip
+	ContentType string `json:"content_type"` // "application/zip"
 }
 
 func (a *aerolab) isLatestVersion() {
@@ -77,10 +87,48 @@ func (a *aerolab) isLatestVersionQuery(v *GitHubRelease, versionFile string) err
 		return err
 	}
 	v.CurrentVersion = vBranch
-	data, err := json.Marshal(v)
-	if err != nil {
-		return nil
+
+	if versionFile != "" {
+		data, err := json.Marshal(v)
+		if err != nil {
+			return nil
+		}
+		os.WriteFile(versionFile, data, 0600)
 	}
-	os.WriteFile(versionFile, data, 0600)
 	return nil
+}
+
+func (a *aerolab) isLatestVersionQueryPrerelease() (*GitHubRelease, error) {
+	client := &http.Client{}
+	client.Timeout = 5 * time.Second
+	defer client.CloseIdleConnections()
+	req, err := http.NewRequest("GET", "https://api.github.com/repos/aerospike/aerolab/releases", nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/vnd.github+json")
+	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+	response, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+	if response.StatusCode != 200 {
+		body, _ := io.ReadAll(response.Body)
+		err = fmt.Errorf("GET 'https://api.github.com/repos/aerospike/aerolab/releases': exit code (%d), message: %s", response.StatusCode, string(body))
+		return nil, err
+	}
+	vPrerelease := []*GitHubRelease{}
+	err = json.NewDecoder(response.Body).Decode(&vPrerelease)
+	if err != nil {
+		return nil, err
+	}
+	for _, pre := range vPrerelease {
+		if !pre.Prerelease {
+			continue
+		}
+		pre.CurrentVersion = vBranch
+		return pre, nil
+	}
+	return nil, errors.New("NOT FOUND")
 }
