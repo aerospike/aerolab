f0028a4d8640141dae5af2d9900ef0ef74853871
Robert Glonek <rglonek@aerospike.com>
2023-12-19 10:53:03 -0800

    fix compile on windows

---
+++ src/cmdAgiExecGetStatus_windows.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/aerospike/aerolab/ingest"
+)
+
+func getAgiStatus(enabled bool, ingestProgressPath string) (*ingest.IngestStatusStruct, error) {
+	return nil, nil
+}
