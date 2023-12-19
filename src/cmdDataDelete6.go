394061d716ee9ad999368d95207e4271db74c7a9
Robert Glonek <rglonek@aerospike.com>
2023-12-19 13:01:28 -0800

    remove v6 aerospike as it won't run with v7

--- src/cmdDataDelete6.go
+++ src/cmdDataDelete6.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/aerospike/aerospike-client-go/v6"
+	"github.com/aerospike/aerospike-client-go/v7"
 )
 
 func (c *dataDeleteCmd) delete6(args []string) error {
