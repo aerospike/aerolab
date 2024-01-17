6dbd8046f97c7c2309641788b6763759c7c361b2
Robert Glonek <rglonek@aerospike.com>
2024-01-17 12:49:53 -0800

    basic menu building and layout handling

--- web/dev/index.js
+++ web/dev/index.js
@@ -1,33 +1,33 @@
 {{define "mainjs"}}
 function menuSetActive(id) {
     // get current item
-    current = $("#"+id);
-    // if current item is an expanding menu item, do exit
-    classList = current.parent().attr("class").split(/\s+/);
-    if (classList.includes("menu-closed")) {
+    let current = $(id);
+    // if current item is an expanding menu item, we are not setting anything; exit
+    if (current.parent().attr("class").split(/\s+/).includes("menu")) {
         return;
     }
-    // remove all active classes
-    menu = $("#mainMenu")
+    // fade in loading spinner
+    $("#loadingSpinner").fadeIn();
+    // reset: remove all active classes
+    let menu = $("#mainMenu")
     menu.find("a").each(function () {
         $(this).removeClass("active");
         $(this).removeClass("bg-blue");
         $(this).removeClass("bg-white");
     });
-    // add active class to current item
+    // add active class to currently selected item
     current.addClass("active");
     current.addClass("bg-blue");
     // recursively add active class to parent menu items
-    menuid = menu.attr("id");
-    while (current.attr("id") != menuid) {
+    let menuId = menu.attr("id");
+    while (current.attr("id") != menuId) {
         current = current.parent();
-        if ($(current).attr("class").split(/\s+/).includes("menu-closed")) {
+        if ($(current).attr("class").split(/\s+/).includes("menu")) {
             $(current).children("a").each(function () {
                 $(this).addClass("active");
                 $(this).addClass("bg-white");
             });
         }
     }
-    // TODO: actually do something in the main section
 }
 {{end}}
