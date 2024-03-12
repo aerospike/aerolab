38c2be8a2b40e487ef7111614b4f517f18bf4f55
Robert Glonek <rglonek@aerospike.com>
2024-03-12 12:35:08 -0700

    switch graph support to v2, support v2 only

--- src/cmdClientDoGraph.go
+++ src/cmdClientDoGraph.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"errors"
 	"fmt"
 	"log"
@@ -25,7 +23,7 @@ type clientCreateGraphCmd struct {
 	Namespace       string          `short:"m" long:"namespace" description:"namespace name to configure graph to use" default:"test"`
 	ExtraProperties []string        `short:"e" long:"extra" description:"extra properties to add; can be specified multiple times; ex: -e 'aerospike.client.timeout=2000'"`
 	AMS             string          `long:"ams" description:"name of an AMS client to add this machine to prometheus configs to"`
-	RAMMb           int             `long:"ram-mb" description:"manually specify amount of RAM MiB to use; default-docker: 4G; default-cloud: 90pct"`
+	RAMMb           int             `long:"ram-mb" description:"manually specify amount of RAM MiB to use"`
 	GraphImage      string          `long:"graph-image" description:"graph is installed using docker images; docker image to use for graph installation" default:"aerospike/aerospike-graph-service"`
 	DockerLoginUser string          `long:"docker-user" description:"login to docker registry for graph installation"`
 	DockerLoginPass string          `long:"docker-pass" description:"login to docker registry for graph installation"`
@@ -121,7 +119,11 @@ func (c *clientCreateGraphCmd) Execute(args []string) error {
 	}
 
 	// continue installation
-	graphConfig := scripts.GetGraphConfig([]string{c.seedip + ":" + c.seedport}, c.Namespace, c.ExtraProperties)
+	if a.opts.Config.Backend.Type == "docker" && c.RAMMb == 0 {
+		log.Println("For local docker deployment, defaulting to 4G RAM limit.")
+		c.RAMMb = 4096
+	}
+	graphConfig := scripts.GetGraphConfig([]string{c.seedip + ":" + c.seedport}, c.Namespace, c.ExtraProperties, c.RAMMb)
 
 	if a.opts.Config.Backend.Type == "docker" {
 		if c.DockerLoginUser != "" && c.DockerLoginPass != "" {
@@ -140,9 +142,6 @@ func (c *clientCreateGraphCmd) Execute(args []string) error {
 			return errors.New("on docker, only one client can be dpeloyed at a time")
 		}
 		// install on docker
-		if c.RAMMb == 0 {
-			c.RAMMb = 4096
-		}
 		curDir, err := os.Getwd()
 		if err != nil {
 			return err
@@ -162,7 +161,7 @@ func (c *clientCreateGraphCmd) Execute(args []string) error {
 		}
 		c.Docker.clientCustomDockerImage = c.GraphImage
 		log.Println("Pulling and running dockerized aerospike-graph, this may take a while...")
-		machines, err := c.createBase([]string{"-e", fmt.Sprintf("JAVA_OPTIONS=-Xmx%dm", c.RAMMb), "-v", confFile + ":/opt/aerospike-graph/conf/aerospike-graph.properties"}, "graph")
+		machines, err := c.createBase([]string{"-v", confFile + ":/opt/conf/aerospike-graph.properties"}, "graph")
 		if err != nil {
 			return err
 		}
@@ -194,43 +193,6 @@ func (c *clientCreateGraphCmd) Execute(args []string) error {
 	}
 	log.Println("Continuing graph installation...")
 	returns := parallelize.MapLimit(machines, c.ParallelThreads, func(node int) error {
-		memSizeMb := c.RAMMb
-		if c.RAMMb == 0 {
-			out, err := b.RunCommands(string(c.ClientName), [][]string{{"free", "-b"}}, []int{node})
-			if err != nil {
-				nout := ""
-				for _, n := range out {
-					nout = nout + "\n" + string(n)
-				}
-				return fmt.Errorf("error on client %s: %s: %s", c.ClusterName, nout, err)
-			}
-			scanner := bufio.NewScanner(bytes.NewReader(out[0]))
-			for scanner.Scan() {
-				line := scanner.Text()
-				if !strings.HasPrefix(line, "Mem:") {
-					continue
-				}
-				memBytes := strings.Fields(line)
-				if len(memBytes) < 2 {
-					return errors.New("memory line corrupt from free -m")
-				}
-				memSizeMb, err = strconv.Atoi(memBytes[1])
-				if err != nil {
-					return fmt.Errorf("could not get memory from %s: %s", memBytes[1], err)
-				}
-				memSizeMb = memSizeMb / 1024 / 1024
-			}
-			if memSizeMb == 0 {
-				return errors.New("could not find memory size from free -b")
-			}
-			sysSizeMb := memSizeMb
-			memSizeMb = int(float64(memSizeMb) * 0.9)
-			if memSizeMb < 1024 {
-				log.Println("RAM size minimum falls below 1GB, setting to 1GB and hoping for the best")
-				memSizeMb = 1024
-			}
-			log.Printf("Client %s Machine %d TotalSizeMB=%d GraphSizeMB=%d", c.ClientName, node, sysSizeMb, memSizeMb)
-		}
 		var graphScript []byte
 		if c.DockerLoginUser != "" && c.DockerLoginPass != "" {
 			graphScript = []byte(fmt.Sprintf("set -e\ndocker login --username '%s' --password '%s'", c.DockerLoginUser, strings.ReplaceAll(c.DockerLoginPass, "'", "\\'")))
@@ -239,7 +201,7 @@ func (c *clientCreateGraphCmd) Execute(args []string) error {
 			}
 			graphScript = append(graphScript, '\n')
 		}
-		graphScript = append(graphScript, scripts.GetCloudGraphScript(memSizeMb, "/etc/aerospike-graph.properties", "", c.GraphImage)...)
+		graphScript = append(graphScript, scripts.GetCloudGraphScript("/etc/aerospike-graph.properties", "", c.GraphImage)...)
 		err = b.CopyFilesToCluster(string(c.ClientName), []fileList{{"/etc/aerospike-graph.properties", string(graphConfig), len(graphConfig)}, {"/tmp/install-graph.sh", string(graphScript), len(graphScript)}}, []int{node})
 		if err != nil {
 			return err
--- src/scripts/graph-start.sh
+++ src/scripts/graph-start.sh
@@ -1,3 +1,3 @@
 # run graph
 set -e
-docker run -itd --label aerolab.client.type=graph --name=%s --restart=always -p8182:8182 -p9090:9090 -e JAVA_OPTIONS="-Xmx%dm" -v %s:/opt/aerospike-graph/conf/aerospike-graph.properties %s %s 
+docker run -itd --label aerolab.client.type=graph --name=%s --restart=always -p8182:8182 -p9090:9090 -v %s:/opt/conf/aerospike-graph.properties %s %s 
