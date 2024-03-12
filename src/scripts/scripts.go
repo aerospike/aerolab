14f5e4f0bc307e3a0aee5cfcfc7e7e9f10e3eb7b
Robert Glonek <rglonek@aerospike.com>
2024-03-12 12:35:08 -0700

    switch graph support to v2, support v2 only

--- src/scripts/scripts.go
+++ src/scripts/scripts.go
@@ -17,12 +17,15 @@ var graphProperties string
 
 // seeds:      []string{"ip:port","ip:port",...}
 // properties: extra properties, for example []string{"aerospike.graph.index.vertex.properties=property1,property2"}
-func GetGraphConfig(seeds []string, namespace string, properties []string) []byte {
+func GetGraphConfig(seeds []string, namespace string, properties []string, rammb int) []byte {
+	if rammb > 0 {
+		properties = append(properties, fmt.Sprintf("aerospike.graph-service.heap.max=%dm", rammb))
+	}
 	return []byte(fmt.Sprintf(graphProperties, strings.Join(seeds, ", "), namespace, strings.Join(properties, "\n")))
 }
 
 // for on-cloud deployments, installs docker and starts graph inside
 // for properties file path, ex: /etc/aerospike-graph/aerospike-graph.properties
-func GetCloudGraphScript(ramMB int, propertiesFilePath string, extraParams string, imageName string) []byte {
-	return append(InstallDocker, []byte(fmt.Sprintf(startGraph, "aerospike-graph", ramMB, propertiesFilePath, extraParams, imageName))...)
+func GetCloudGraphScript(propertiesFilePath string, extraParams string, imageName string) []byte {
+	return append(InstallDocker, []byte(fmt.Sprintf(startGraph, "aerospike-graph", propertiesFilePath, extraParams, imageName))...)
 }
