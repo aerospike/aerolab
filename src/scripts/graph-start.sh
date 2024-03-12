34f003389a1a36f8642a0da776a48fd149524575
Robert Glonek <rglonek@aerospike.com>
2024-03-12 10:17:27 -0700

    custom docker image for client graph - scripts

--- src/scripts/graph-start.sh
+++ src/scripts/graph-start.sh
@@ -1,3 +1,3 @@
 # run graph
 set -e
-docker run -itd --label aerolab.client.type=graph --name=%s --restart=always -p8182:8182 -p9090:9090 -e JAVA_OPTIONS="-Xmx%dm" -v %s:/opt/aerospike-graph/conf/aerospike-graph.properties %s aerospike/aerospike-graph-service 
+docker run -itd --label aerolab.client.type=graph --name=%s --restart=always -p8182:8182 -p9090:9090 -e JAVA_OPTIONS="-Xmx%dm" -v %s:/opt/aerospike-graph/conf/aerospike-graph.properties %s %s 
