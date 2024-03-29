f823620b2a4aac70f467051658411d20384dffa0
Robert Glonek <rglonek@aerospike.com>
2024-03-29 16:36:32 -0700

    work on vector installation and config patching logic

--- src/cmdClientDoVector.go
+++ src/cmdClientDoVector.go
@@ -5,35 +5,28 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"os"
-	"os/exec"
-	"path/filepath"
 	"strconv"
 	"strings"
 
 	"github.com/aerospike/aerolab/parallelize"
-	"github.com/aerospike/aerolab/scripts"
 	"github.com/bestmethod/inslice"
+	"github.com/jessevdk/go-flags"
 )
-
-// TODO make this optionally configurable or change to 8185 or something like that
-const vectorAccessPort = "5000"
-const vectorAccessProtocol = "http"
 
 type clientCreateVectorCmd struct {
 	clientCreateNoneCmd
-	ClusterName     TypeClusterName `short:"C" long:"cluster-name" description:"cluster name to seed from" default:"mydc"`
-	Seed            string          `long:"seed" description:"specify a seed IP:PORT instead of providing a ClusterName; if this parameter is provided, ClusterName is ignored"`
-	Namespace       string          `short:"m" long:"namespace" description:"namespace name to configure graph to use" default:"test"`
-	ExtraProperties []string        `short:"e" long:"extra" description:"extra properties to add; can be specified multiple times; ex: -e 'aerospike.client.timeout=2000'"`
-	RAMMb           int             `long:"ram-mb" description:"manually specify amount of RAM MiB to use"`
-	GraphImage      string          `long:"graph-image" description:"graph is installed using docker images; docker image to use for graph installation" default:"aerospike/aerospike-graph-service"`
-	DockerLoginUser string          `long:"docker-user" description:"login to docker registry for graph installation"`
-	DockerLoginPass string          `long:"docker-pass" description:"login to docker registry for graph installation"`
-	DockerLoginURL  string          `long:"docker-url" description:"login to docker registry for graph installation"`
-	JustDoIt        bool            `long:"confirm" description:"set this parameter to confirm any warning questions without being asked to press ENTER to continue" webdisable:"true" webset:"true"`
-	seedip          string
-	seedport        string
+	ClusterName          TypeClusterName `short:"C" long:"cluster-name" description:"cluster name to seed from" default:"mydc"`
+	Seed                 string          `long:"seed" description:"specify a seed IP:PORT instead of providing a ClusterName; if this parameter is provided, ClusterName is ignored"`
+	ServiceListen        string          `long:"listen" description:"specify a listen IP:PORT for the service" default:"0.0.0.0:5000"`
+	NoTouchServiceListen bool            `long:"no-touch-listen" description:"set this to prevent aerolab from touching the service: configuration part"`
+	NoTouchSeed          bool            `long:"no-touch-seed" description:"set this to prevent aerolab from configuring the aerospike seed ip and port"`
+	JustDoIt             bool            `long:"confirm" description:"set this parameter to confirm any warning questions without being asked to press ENTER to continue" webdisable:"true" webset:"true"`
+	VectorVersion        string          `long:"version" description:"vector version to install; only 0.3.1 is officially supported by aerolab" default:"0.3.1"`
+	CustomConf           flags.Filename  `long:"custom-conf" description:"provide a custom aerospike-proximus.yml to ship"`
+	seedip               string
+	seedport             string
+	serviceip            string
+	serviceport          string
 	chDirCmd
 }
 
@@ -41,18 +34,30 @@ func (c *clientCreateVectorCmd) Execute(args []string) error {
 	if earlyProcess(args) {
 		return nil
 	}
-	if a.opts.Config.Backend.Type == "docker" && !strings.Contains(c.Docker.ExposePortsToHost, ":"+vectorAccessPort) {
+	if !c.NoTouchServiceListen {
+		addr, err := net.ResolveTCPAddr("tcp", c.ServiceListen)
+		if err != nil {
+			return err
+		}
+		c.serviceport = strconv.Itoa(addr.Port)
+		c.serviceip = addr.IP.String()
+	} else {
+		c.serviceport = "5000"
+		c.serviceip = "localhost"
+	}
+
+	if a.opts.Config.Backend.Type == "docker" && !strings.Contains(c.Docker.ExposePortsToHost, ":"+c.serviceport) {
 		if c.Docker.NoAutoExpose {
-			fmt.Println("Docker backend is in use, but vector access port is not being forwarded. If using Docker Desktop, use '-e " + vectorAccessPort + ":" + vectorAccessPort + "' parameter in order to forward port " + vectorAccessPort + ". Press ENTER to continue regardless.")
+			fmt.Println("Docker backend is in use, but vector access port is not being forwarded. If using Docker Desktop, use '-e " + c.serviceport + ":" + c.serviceport + "' parameter in order to forward port " + c.serviceport + ". Press ENTER to continue regardless.")
 			if !c.JustDoIt {
 				var ignoreMe string
 				fmt.Scanln(&ignoreMe)
 			}
 		} else {
-			c.Docker.ExposePortsToHost = strings.Trim(vectorAccessPort+":"+vectorAccessPort+","+c.Docker.ExposePortsToHost, ",")
+			c.Docker.ExposePortsToHost = strings.Trim(c.serviceport+":"+c.serviceport+","+c.Docker.ExposePortsToHost, ",")
 		}
 	}
-	if c.Seed == "" {
+	if c.Seed == "" && !c.NoTouchSeed {
 		fmt.Println("Getting cluster list")
 		b.WorkOnServers()
 		clist, err := b.ClusterList()
@@ -96,7 +101,7 @@ func (c *clientCreateVectorCmd) Execute(args []string) error {
 				}
 			}
 		}
-	} else {
+	} else if !c.NoTouchSeed {
 		addr, err := net.ResolveTCPAddr("tcp", c.Seed)
 		if err != nil {
 			return err
@@ -105,67 +110,14 @@ func (c *clientCreateVectorCmd) Execute(args []string) error {
 		c.seedip = addr.IP.String()
 	}
 	b.WorkOnClients()
-	if c.seedip == "" {
+	if !c.NoTouchSeed && c.seedip == "" {
 		return errors.New("could not find an IP for a node in the given cluster - are all the nodes down?")
 	}
 
-	// TODO continue installation
-	return errors.New("not implemented yet")
-	if a.opts.Config.Backend.Type == "docker" && c.RAMMb == 0 {
-		log.Println("For local docker deployment, defaulting to 4G RAM limit.")
-		c.RAMMb = 4096
-	}
-	graphConfig := scripts.GetGraphConfig([]string{c.seedip + ":" + c.seedport}, c.Namespace, c.ExtraProperties, c.RAMMb)
-
-	if a.opts.Config.Backend.Type == "docker" {
-		if c.DockerLoginUser != "" && c.DockerLoginPass != "" {
-			log.Println("Docker Login...")
-			params := []string{"login", "--username", c.DockerLoginUser, "--password", c.DockerLoginPass}
-			if c.DockerLoginURL != "" {
-				params = append(params, c.DockerLoginURL)
-			}
-			out, err := exec.Command("docker", params...).CombinedOutput()
-			if err != nil {
-				return fmt.Errorf("%s\n%s", err, string(out))
-			}
-		}
-		log.Println("Running client.create.vector")
-		if c.ClientCount > 1 {
-			return errors.New("on docker, only one client can be dpeloyed at a time")
-		}
-		// install on docker
-		curDir, err := os.Getwd()
-		if err != nil {
-			return err
-		}
-		confFile := filepath.Join(curDir, "aerospike-graph.properties")
-		if _, err := os.Stat(confFile); err == nil {
-			log.Printf("WARNING: configuration file %s already exists. Press ENTER to override and continue.", confFile)
-			if !c.JustDoIt {
-				var ignoreMe string
-				fmt.Scanln(&ignoreMe)
-			}
-		}
-		log.Printf("Saving configuration file to %s", confFile)
-		err = os.WriteFile(confFile, graphConfig, 0644)
-		if err != nil {
-			return err
-		}
-		c.Docker.clientCustomDockerImage = c.GraphImage
-		log.Println("Pulling and running dockerized aerospike-vector, this may take a while...")
-		_, err = c.createBase([]string{"-v", confFile + ":/opt/aerospike-graph/aerospike-graph.properties"}, "graph")
-		if err != nil {
-			return err
-		}
-		log.Print("Done")
-		log.Print("\nCommon tasks and commands:")
-		log.Print(" * access gremlin console:          docker run -it --rm tinkerpop/gremlin-console")
-		log.Printf(" * access terminal on graph server: aerolab attach client -n %s", c.ClientName)
-		log.Print(" * visit https://gdotv.com/ to download a Graph IDE and Visualization tool")
-		return nil
-	}
-
-	// install on cloud
+	// install
+	c.Docker.Labels = append(c.Docker.Labels, "lport="+c.serviceport)
+	c.Aws.Tags = append(c.Aws.Tags, "lport="+c.serviceport)
+	c.Gcp.Labels = append(c.Gcp.Labels, "lport="+c.serviceport)
 	machines, err := c.createBase(args, "vector")
 	if err != nil {
 		return err
@@ -175,26 +127,111 @@ func (c *clientCreateVectorCmd) Execute(args []string) error {
 	}
 	log.Println("Continuing vector installation...")
 	returns := parallelize.MapLimit(machines, c.ParallelThreads, func(node int) error {
-		var dockerLogin *scripts.DockerLogin
-		if c.DockerLoginUser != "" && c.DockerLoginPass != "" {
-			dockerLogin = &scripts.DockerLogin{
-				URL:  c.DockerLoginURL,
-				User: c.DockerLoginUser,
-				Pass: c.DockerLoginPass,
+		/* TODO: advertised listeners - valid only for docker - if running via expose, set to 127.0.0.1
+		# The Proximus service listening ports, TLS and network interface.
+		service:
+		  ports:
+		    5000: {}
+		  # Required when running behind NAT
+		  advertised-listeners:
+		   default:
+		     # List of externally accessible addresses and ports for this Proximus instance.
+		     - address: {EXTERNAL_IP}
+		       port: 5000
+
+		*/
+		// TODO make version string configurable
+		/* NOTES
+		Vector download one of:
+		 * https://aerospike.jfrog.io/artifactory/deb/aerospike-proximus-0.3.1.deb
+		 * https://aerospike.jfrog.io/artifactory/rpm/aerospike-proximus-0.3.1-1.noarch.rpm
+
+		set -e
+		[ -f /tmp/installer.deb ] && apt update && apt -y install openjdk-21-jdk-headless /tmp/installer.deb
+		[ -f /tmp/installer.rpm ] && yum -y install java-21-openjdk && yum localinstall /tmp/installer.rpm
+		aerolab cluster attach -n $NEW_TRIAL_NAME -- systemctl enable --now aerospike-proximus
+
+		Cloud:
+		 * systemctl enable --now aerospike-proximus
+		Docker:
+		 * /opt/aerospike-proximus/bin/aerospike-proximus -f /etc/aerospike-proximus/aerospike-proximus.yml
+
+		custom /etc/aerospike-proximus/aerospike-proximus.yml
+		custom /etc/aerospike-proximus
+		port 5000 - allow custom configuration
+
+		version - allow custom
+
+		auto-patch yaml to change port:
+		config["service"]["ports"] = map[string]interface{}{
+			"5000": map[string]interface{}{
+				"addresses": []string{
+					"0.0.0.0"
+				},
+			},
+		}
+		--
+		config["aerospike"]["seeds"] = []map[string]interface{}{
+			map[string]interface{}{
+				"1.2.3.4": map[string]interface{}{
+					"port": 3000,
+				},
+			},
+		}
+		*/
+		/* TODO b.CopyFilesToCluster
+		optional custom yaml configuration file OR option custom whole configuration directory
+		upload the installer-download and installation script
+			err = b.CopyFilesToCluster(string(c.ClientName), []fileList{{"/etc/aerospike-graph.properties", string(graphConfig), len(graphConfig)}, {"/tmp/install-graph.sh", string(graphScript), len(graphScript)}}, []int{node})
+			if err != nil {
+				return err
 			}
-		}
-		graphScript := scripts.GetCloudGraphScript("/etc/aerospike-graph.properties", "", c.GraphImage, dockerLogin)
-		err = b.CopyFilesToCluster(string(c.ClientName), []fileList{{"/etc/aerospike-graph.properties", string(graphConfig), len(graphConfig)}, {"/tmp/install-graph.sh", string(graphScript), len(graphScript)}}, []int{node})
+		*/
+		/* TODO a.opts.Attach
+		run installation script (it should contain, for docker only, the /opt/autoload vector starter)
+			a.opts.Attach.Client.ClientName = c.ClientName
+			a.opts.Attach.Client.Machine = TypeMachines(strconv.Itoa(node))
+			defer backendRestoreTerminal()
+			err = a.opts.Attach.Client.run([]string{"/bin/bash", "/tmp/install-graph.sh"})
+			if err != nil {
+				return err
+			}
+		*/
+		/* TODO download/path/upload
+		patch configuration file with service listen and aerospike seed ip/ports
+		*/
+		/* TODO run the service (docker/cloud specific run command)
+		 */
+		/* YAML PATCHER
+		config := make(map[interface{}]interface{})
+		err := yaml.NewDecoder(strings.NewReader(yml)).Decode(config)
 		if err != nil {
-			return err
+			log.Fatal(err)
 		}
-		a.opts.Attach.Client.ClientName = c.ClientName
-		a.opts.Attach.Client.Machine = TypeMachines(strconv.Itoa(node))
-		defer backendRestoreTerminal()
-		err = a.opts.Attach.Client.run([]string{"/bin/bash", "/tmp/install-graph.sh"})
+		err = mapDelete(config, []interface{}{"service", "ports"})
 		if err != nil {
-			return err
+			log.Fatal(err)
 		}
+		err = mapMakeSet(config, []interface{}{"service", "ports", 6000, "addresses"}, []string{"0.0.0.0"})
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = mapDelete(config, []interface{}{"aerospike", "seeds"})
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = mapMakeSet(config, []interface{}{"aerospike", "seeds"}, []map[string]interface{}{
+			{
+				"1.2.3.4": map[string]int{
+					"port": 3000,
+				},
+			},
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+		yaml.NewEncoder(os.Stdout).Encode(config)
+		*/
 		return nil
 	})
 	isError := false
@@ -208,15 +245,45 @@ func (c *clientCreateVectorCmd) Execute(args []string) error {
 		return errors.New("some nodes returned errors")
 	}
 	log.Println("Done")
-	log.Print("Common tasks and commands:")
-	log.Print(" * access gremlin console locally:         docker run -it --rm tinkerpop/gremlin-console")
-	log.Printf(" * access gremlin console on graph server: aerolab attach client -n %s -- docker run -it --rm tinkerpop/gremlin-console", c.ClientName)
-	log.Printf(" * access terminal on graph server:        aerolab attach client -n %s", c.ClientName)
-	log.Print(" * visit https://gdotv.com/ to download a Graph IDE and Visualization tool")
-	log.Print("Example creating a dedicated gremlin-console client:")
-	log.Print(" * create an empty client: aerolab client create base -n mygremlin [...]")
-	log.Print(" * donwload docker script: aerolab client attach -n mygremlin -- curl -fsSL https://get.docker.com -o /tmp/get-docker.sh")
-	log.Print(" * install docker        : aerolab client attach -n mygremlin -- bash /tmp/get-docker.sh")
-	log.Print(" * run gremlin-console   : aerolab client attach -n mygremlin -- docker run -it --rm tinkerpop/gremlin-console")
+	log.Println(" * Proximus python client manual: https://github.com/aerospike/aerospike-proximus-client-python")
+	log.Println(" * Proximus usage examples:       https://github.com/aerospike/proximus-examples")
+	return nil
+}
+
+func mapDelete(mp interface{}, keys []interface{}) error {
+	for i, key := range keys {
+		switch m := mp.(type) {
+		case map[interface{}]interface{}:
+			if i < len(keys)-1 {
+				if _, ok := m[key]; !ok {
+					return nil
+				}
+			} else {
+				delete(m, key)
+			}
+			mp = m[key]
+		default:
+			return errors.New("invalid map type, must be map[string]interface{}")
+		}
+	}
+	return nil
+}
+
+func mapMakeSet(mp interface{}, keys []interface{}, value interface{}) error {
+	for i, key := range keys {
+		switch m := mp.(type) {
+		case map[interface{}]interface{}:
+			if i < len(keys)-1 {
+				if _, ok := m[key]; !ok {
+					m[key] = make(map[interface{}]interface{})
+				}
+			} else {
+				m[key] = value
+			}
+			mp = m[key]
+		default:
+			return errors.New("invalid map type, must be map[interface{}]interface{}")
+		}
+	}
 	return nil
 }
