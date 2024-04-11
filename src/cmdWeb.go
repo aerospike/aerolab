d72c10eaf10b1c82b0647d8715afccd7d29b51f3
Robert Glonek <rglonek@aerospike.com>
2024-04-11 16:38:54 -0700

    WebUI: always show current backend in the top

--- src/cmdWeb.go
+++ src/cmdWeb.go
@@ -1377,7 +1377,13 @@ func (c *webCmd) serve(w http.ResponseWriter, r *http.Request) {
 		errTitle = "Failed to generate form items"
 		isError = true
 	}
-
+	backendIcon := "fa-aws"
+	if a.opts.Config.Backend.Type == "gcp" {
+		backendIcon = "fa-google"
+	}
+	if a.opts.Config.Backend.Type == "docker" {
+		backendIcon = "fa-docker"
+	}
 	p := &webui.Page{
 		WebRoot:                                 c.WebRoot,
 		FixedNavbar:                             true,
@@ -1396,6 +1402,10 @@ func (c *webCmd) serve(w http.ResponseWriter, r *http.Request) {
 				{
 					Name: "Home",
 					Href: c.WebRoot,
+				},
+				{
+					Name: "<i class=\"fa-brands " + backendIcon + "\"></i>",
+					Href: strings.TrimRight(c.WebRoot, "/") + "/config/backend",
 				},
 			},
 		},
