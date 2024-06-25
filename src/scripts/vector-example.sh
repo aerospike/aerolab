2a826af36fa1360d421c3debf0bb8b1a07b05e54
Robert Glonek <rglonek@aerospike.com>
2024-06-25 14:36:36 -0700

    vector prism numpy version fix

--- src/scripts/vector-example.sh
+++ src/scripts/vector-example.sh
@@ -4,14 +4,17 @@ then
   apt -y install python3 python3-pip git
   cd /opt && git clone https://github.com/aerospike/proximus-examples.git
   cd /opt/proximus-examples/prism-image-search/prism
-  python3 -m pip install -r requirements.txt --extra-index-url https://aerospike.jfrog.io/artifactory/api/pypi/aerospike-pypi-dev/simple
+  python3 -m pip install -r requirements.txt
   mkdir -p /opt/proximus-examples/prism-image-search/prism/static/images/data/
+  set +e
+  python3 -m pip uninstall -y numpy
+  set -e
+  python3 -m pip install -Iv numpy==1.26.4
   touch /opt/prism-example-installed
 fi
 if [ "$1" != "install" ]
 then
   set -e
-  export PROXIMUS_PORT=%s
   export AVS_PORT=%s
   cd /opt/proximus-examples/prism-image-search/prism
   waitress-serve --host 0.0.0.0 --port %s --threads 32 prism:app
