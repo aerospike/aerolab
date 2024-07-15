167c84134dcf31f61e3c8b5c9594ab007fdf32ba
Robert Glonek <rglonek@aerospike.com>
2024-07-15 12:31:47 -0700

    implement sigint handler for windows

--- src/sigintkill_windows.go
+++ src/sigintkill_windows.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"syscall"
 )
@@ -14,4 +15,20 @@ func exitNow() {
 	if err != nil {
 		os.Exit(1)
 	}
+}
+
+func sendSigInt(pid int) error {
+	d, e := syscall.LoadDLL("kernel32.dll")
+	if e != nil {
+		return fmt.Errorf("LoadDLL: %v\n", e)
+	}
+	p, e := d.FindProc("GenerateConsoleCtrlEvent")
+	if e != nil {
+		return fmt.Errorf("FindProc: %v\n", e)
+	}
+	r, _, e := p.Call(syscall.CTRL_BREAK_EVENT, uintptr(pid))
+	if r == 0 {
+		return fmt.Errorf("GenerateConsoleCtrlEvent: %v\n", e)
+	}
+	return nil
 }
