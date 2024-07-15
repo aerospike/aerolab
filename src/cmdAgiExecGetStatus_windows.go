4c2c6b78fd66b5803a1100375419a3bb5bac2af7
Robert Glonek <rglonek@aerospike.com>
2024-07-15 12:36:20 -0700

    small improvement - warning on windows compile

--- src/cmdAgiExecGetStatus_windows.go
+++ src/cmdAgiExecGetStatus_windows.go
@@ -7,6 +7,6 @@ import (
 	"github.com/aerospike/aerolab/ingest"
 )
 
-func getAgiStatus(enabled bool, ingestProgressPath string) (*ingest.IngestStatusStruct, error) {
+func getAgiStatus(bool, string) (*ingest.IngestStatusStruct, error) {
 	return nil, nil
 }
