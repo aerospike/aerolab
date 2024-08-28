caef585b733cfa056e2d98cb8fdd8290eac653ab
Robert Glonek <rglonek@aerospike.com>
2024-08-28 10:40:31 -0700

    add port 8081 to default open ports mapping

--- src/backendGcp.go
+++ src/backendGcp.go
@@ -2924,7 +2924,7 @@ func (d *backendGcp) CreateSecurityGroups(vpc string, namePrefix string, isAgi b
 }
 
 func (d *backendGcp) createSecurityGroupExternal(namePrefix string, isAgi bool, extraPorts []string, noDefaults bool) error {
-	ports := []string{"22", "3000", "443", "80", "8080", "8888", "9200", "8182", "8998"}
+	ports := []string{"22", "3000", "443", "80", "8080", "8888", "9200", "8182", "8998", "8081"}
 	if isAgi {
 		ports = []string{"22", "80", "443"}
 	}
@@ -3021,7 +3021,7 @@ func (d *backendGcp) createSecurityGroupInternal() error {
 }
 
 func (d *backendGcp) LockSecurityGroups(ip string, lockSSH bool, vpc string, namePrefix string, isAgi bool, extraPorts []string, noDefaults bool) error {
-	ports := []string{"22", "3000", "443", "80", "8080", "8888", "9200", "8182", "8998"}
+	ports := []string{"22", "3000", "443", "80", "8080", "8888", "9200", "8182", "8998", "8081"}
 	if isAgi {
 		ports = []string{"22", "80", "443"}
 	}
