2677ce2987555e4950d3d4f2c59a9fb16ffba956
Robert Glonek <rglonek@aerospike.com>
2024-08-28 10:40:14 -0700

    support raw binary download of files via ssh for logs get feature

--- src/backend_darwin.go
+++ src/backend_darwin.go
@@ -39,49 +39,51 @@ func (ssh_client *SSH) RunAttachCmd(cmd string, stdin io.Reader, stdout io.Write
 	ssh_client.session.Stdin = stdin
 	ssh_client.session.Stdout = stdout
 	ssh_client.session.Stderr = stderr
-	fileDescriptor := int(os.Stdout.Fd())
-	var err error
 	modes := ssh.TerminalModes{
 		ssh.ECHO:          1,     // enable echoing
 		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
 		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
 	}
-	if term.IsTerminal(fileDescriptor) {
-		restoreTerminalLock.Lock()
-		if restoreTerminalState == nil && isInteractive {
-			originalState, err := term.MakeRaw(fileDescriptor)
+	var err error
+	if stdout == os.Stdout {
+		fileDescriptor := int(os.Stdout.Fd())
+		if term.IsTerminal(fileDescriptor) {
+			restoreTerminalLock.Lock()
+			if restoreTerminalState == nil && isInteractive {
+				originalState, err := term.MakeRaw(fileDescriptor)
+				if err != nil {
+					return err
+				}
+				restoreTerminalState = originalState
+			}
+			restoreTerminalLock.Unlock()
+
+			termWidth, termHeight, err := term.GetSize(fileDescriptor)
 			if err != nil {
 				return err
 			}
-			restoreTerminalState = originalState
-		}
-		restoreTerminalLock.Unlock()
 
-		termWidth, termHeight, err := term.GetSize(fileDescriptor)
-		if err != nil {
-			return err
-		}
-
-		err = ssh_client.session.RequestPty("xterm-256color", termHeight, termWidth, modes)
-		if err != nil {
-			return err
-		}
-	} else {
-		err = ssh_client.session.RequestPty("vt100", 24, 80, modes)
-		if err != nil {
-			return err
-		}
-	}
-	go func() {
-		sigs := make(chan os.Signal, 1)
-		signal.Notify(sigs, syscall.SIGWINCH)
-		defer signal.Stop(sigs)
-		for range sigs {
-			if _, err := ssh_client.session.SendRequest("window-change", false, termSize(os.Stdin.Fd())); err != nil {
-				log.Print(err)
+			err = ssh_client.session.RequestPty("xterm-256color", termHeight, termWidth, modes)
+			if err != nil {
+				return err
+			}
+		} else {
+			err = ssh_client.session.RequestPty("vt100", 24, 80, modes)
+			if err != nil {
+				return err
 			}
 		}
-	}()
+		go func() {
+			sigs := make(chan os.Signal, 1)
+			signal.Notify(sigs, syscall.SIGWINCH)
+			defer signal.Stop(sigs)
+			for range sigs {
+				if _, err := ssh_client.session.SendRequest("window-change", false, termSize(os.Stdin.Fd())); err != nil {
+					log.Print(err)
+				}
+			}
+		}()
+	}
 	err = ssh_client.session.Run(cmd)
 	return err
 }
--- src/backend_windows.go
+++ src/backend_windows.go
@@ -37,7 +37,6 @@ func (ssh_client *SSH) RunAttachCmd(cmd string, stdin io.Reader, stdout io.Write
 	ssh_client.session.Stdin = stdin
 	ssh_client.session.Stdout = stdout
 	ssh_client.session.Stderr = stderr
-	fileDescriptor := int(os.Stdout.Fd())
 	var err error
 	termWidth := 80
 	termHeight := 24
@@ -46,62 +45,65 @@ func (ssh_client *SSH) RunAttachCmd(cmd string, stdin io.Reader, stdout io.Write
 		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
 		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
 	}
-	if term.IsTerminal(fileDescriptor) {
-		termWidth, termHeight, err = term.GetSize(fileDescriptor)
-		if err != nil {
-			return err
-		}
-
-		err = ssh_client.session.RequestPty("vt100", termHeight, termWidth, modes)
-		if err != nil {
-			return err
-		}
-	} else {
-		err = ssh_client.session.RequestPty("vt100", 24, 80, modes)
-		if err != nil {
-			return err
-		}
-	}
-	restoreTerminalLock.Lock()
-	if restoreTerminalState == nil && isInteractive {
-		current := console.Current()
-		defer current.Reset()
-
-		if err := current.SetRaw(); err != nil {
-		}
-		ws, err := current.Size()
-		if err != nil {
-		}
-		current.Resize(ws)
-		restoreTerminalState = &current
-		isExit := make(chan struct{}, 1)
-		defer func() {
-			isExit <- struct{}{}
-		}()
-		go func() {
-			var oldSize console.WinSize
-			for {
-				time.Sleep(time.Second)
-				winSize, err := current.Size()
-				if err != nil {
-					continue
-				}
-				if oldSize.Height == 0 && oldSize.Width == 0 {
-					oldSize = winSize
-					continue
-				}
-				if oldSize.Height == winSize.Height && oldSize.Width == winSize.Width {
-					continue
-				}
-				if _, err := ssh_client.session.SendRequest("window-change", false, termSize(os.Stdout.Fd())); err != nil {
-					log.Print(err)
-				} else {
-					oldSize = winSize
-				}
+	if stdout == os.Stdout {
+		fileDescriptor := int(os.Stdout.Fd())
+		if term.IsTerminal(fileDescriptor) {
+			termWidth, termHeight, err = term.GetSize(fileDescriptor)
+			if err != nil {
+				return err
 			}
-		}()
+
+			err = ssh_client.session.RequestPty("vt100", termHeight, termWidth, modes)
+			if err != nil {
+				return err
+			}
+		} else {
+			err = ssh_client.session.RequestPty("vt100", 24, 80, modes)
+			if err != nil {
+				return err
+			}
+		}
+		restoreTerminalLock.Lock()
+		if restoreTerminalState == nil && isInteractive {
+			current := console.Current()
+			defer current.Reset()
+
+			if err := current.SetRaw(); err != nil {
+			}
+			ws, err := current.Size()
+			if err != nil {
+			}
+			current.Resize(ws)
+			restoreTerminalState = &current
+			isExit := make(chan struct{}, 1)
+			defer func() {
+				isExit <- struct{}{}
+			}()
+			go func() {
+				var oldSize console.WinSize
+				for {
+					time.Sleep(time.Second)
+					winSize, err := current.Size()
+					if err != nil {
+						continue
+					}
+					if oldSize.Height == 0 && oldSize.Width == 0 {
+						oldSize = winSize
+						continue
+					}
+					if oldSize.Height == winSize.Height && oldSize.Width == winSize.Width {
+						continue
+					}
+					if _, err := ssh_client.session.SendRequest("window-change", false, termSize(os.Stdout.Fd())); err != nil {
+						log.Print(err)
+					} else {
+						oldSize = winSize
+					}
+				}
+			}()
+		}
+		restoreTerminalLock.Unlock()
 	}
-	restoreTerminalLock.Unlock()
 	err = ssh_client.session.Run(cmd)
 	return err
 }
