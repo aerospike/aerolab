5eab68b05bfa656d59c8f65544491f0ec7228675
Robert Glonek <rglonek@aerospike.com>
2026-02-13 12:16:56 -0700

    eks expiry - fix

--- src/pkg/expiry/expire/expire.go
+++ src/pkg/expiry/expire/expire.go
@@ -237,7 +237,7 @@ func (h *ExpiryHandler) expireEksctl(region string) error {
 		for _, stack := range stackList {
 			log.Printf("EKS: Disabling termination protection on stack %s", stack.Name)
 			_, err = cfSvc.UpdateTerminationProtection(context.TODO(), &cloudformation.UpdateTerminationProtectionInput{
-				StackName:                  aws.String(stack.Name),
+				StackName:                   aws.String(stack.Name),
 				EnableTerminationProtection: aws.Bool(false),
 			})
 			if err != nil {
@@ -314,6 +314,163 @@ func (h *ExpiryHandler) expireEksctl(region string) error {
 					})
 					if err != nil {
 						return fmt.Errorf("could not iam.DeleteOpenIDConnectProvider on %s: %w", oidcDel, err)
+					}
+				}
+			}
+			// find the VPCs associated with the stack; then delete their security group, routes, and peerings, in that order
+			// delete the stack after the extra VPC resources have been deleted
+			log.Printf("EKS: Describing resources for stack %s to find VPCs", stack.Name)
+			stackResources, err := cfSvc.DescribeStackResources(context.TODO(), &cloudformation.DescribeStackResourcesInput{
+				StackName: aws.String(stack.Name),
+			})
+			if err != nil {
+				log.Printf("EKS: Warning: could not describe stack resources for %s: %s", stack.Name, err)
+			} else {
+				// collect VPC IDs from the stack resources
+				var vpcIds []string
+				for _, res := range stackResources.StackResources {
+					if aws.ToString(res.ResourceType) == "AWS::EC2::VPC" && res.PhysicalResourceId != nil {
+						vpcIds = append(vpcIds, aws.ToString(res.PhysicalResourceId))
+					}
+				}
+				for _, vpcId := range vpcIds {
+					log.Printf("EKS: Cleaning up VPC resources for %s in stack %s", vpcId, stack.Name)
+
+					// 1. Delete non-default security groups in the VPC
+					sgOut, err := ec2svc.DescribeSecurityGroups(context.TODO(), &ec2.DescribeSecurityGroupsInput{
+						Filters: []ec2types.Filter{
+							{
+								Name:   aws.String("vpc-id"),
+								Values: []string{vpcId},
+							},
+						},
+					})
+					if err != nil {
+						log.Printf("EKS: Warning: could not describe security groups for VPC %s: %s", vpcId, err)
+					} else {
+						// first revoke all ingress/egress rules that reference non-default SGs, to avoid dependency issues
+						for _, sg := range sgOut.SecurityGroups {
+							if aws.ToString(sg.GroupName) == "default" {
+								continue
+							}
+							if len(sg.IpPermissions) > 0 {
+								_, err = ec2svc.RevokeSecurityGroupIngress(context.TODO(), &ec2.RevokeSecurityGroupIngressInput{
+									GroupId:       sg.GroupId,
+									IpPermissions: sg.IpPermissions,
+								})
+								if err != nil {
+									log.Printf("EKS: Warning: could not revoke ingress rules for SG %s: %s", aws.ToString(sg.GroupId), err)
+								}
+							}
+							if len(sg.IpPermissionsEgress) > 0 {
+								_, err = ec2svc.RevokeSecurityGroupEgress(context.TODO(), &ec2.RevokeSecurityGroupEgressInput{
+									GroupId:       sg.GroupId,
+									IpPermissions: sg.IpPermissionsEgress,
+								})
+								if err != nil {
+									log.Printf("EKS: Warning: could not revoke egress rules for SG %s: %s", aws.ToString(sg.GroupId), err)
+								}
+							}
+						}
+						// now delete the non-default security groups
+						for _, sg := range sgOut.SecurityGroups {
+							if aws.ToString(sg.GroupName) == "default" {
+								continue
+							}
+							log.Printf("EKS: Deleting security group %s (%s) in VPC %s", aws.ToString(sg.GroupId), aws.ToString(sg.GroupName), vpcId)
+							_, err = ec2svc.DeleteSecurityGroup(context.TODO(), &ec2.DeleteSecurityGroupInput{
+								GroupId: sg.GroupId,
+							})
+							if err != nil {
+								log.Printf("EKS: Warning: could not delete security group %s: %s", aws.ToString(sg.GroupId), err)
+							}
+						}
+					}
+
+					// 2. Delete routes referencing VPC peering connections in the VPC's route tables
+					rtOut, err := ec2svc.DescribeRouteTables(context.TODO(), &ec2.DescribeRouteTablesInput{
+						Filters: []ec2types.Filter{
+							{
+								Name:   aws.String("vpc-id"),
+								Values: []string{vpcId},
+							},
+						},
+					})
+					if err != nil {
+						log.Printf("EKS: Warning: could not describe route tables for VPC %s: %s", vpcId, err)
+					} else {
+						for _, rt := range rtOut.RouteTables {
+							for _, route := range rt.Routes {
+								if route.VpcPeeringConnectionId == nil {
+									continue
+								}
+								cidr := aws.ToString(route.DestinationCidrBlock)
+								if cidr == "" {
+									cidr = aws.ToString(route.DestinationIpv6CidrBlock)
+								}
+								if cidr == "" {
+									continue
+								}
+								log.Printf("EKS: Deleting route %s -> peering %s in route table %s", cidr, aws.ToString(route.VpcPeeringConnectionId), aws.ToString(rt.RouteTableId))
+								deleteRouteInput := &ec2.DeleteRouteInput{
+									RouteTableId: rt.RouteTableId,
+								}
+								if route.DestinationCidrBlock != nil {
+									deleteRouteInput.DestinationCidrBlock = route.DestinationCidrBlock
+								} else {
+									deleteRouteInput.DestinationIpv6CidrBlock = route.DestinationIpv6CidrBlock
+								}
+								_, err = ec2svc.DeleteRoute(context.TODO(), deleteRouteInput)
+								if err != nil {
+									log.Printf("EKS: Warning: could not delete route %s in route table %s: %s", cidr, aws.ToString(rt.RouteTableId), err)
+								}
+							}
+						}
+					}
+
+					// 3. Delete VPC peering connections associated with this VPC
+					peeringOut, err := ec2svc.DescribeVpcPeeringConnections(context.TODO(), &ec2.DescribeVpcPeeringConnectionsInput{
+						Filters: []ec2types.Filter{
+							{
+								Name:   aws.String("requester-vpc-info.vpc-id"),
+								Values: []string{vpcId},
+							},
+						},
+					})
+					if err != nil {
+						log.Printf("EKS: Warning: could not describe VPC peering connections for VPC %s: %s", vpcId, err)
+					} else {
+						for _, peering := range peeringOut.VpcPeeringConnections {
+							log.Printf("EKS: Deleting VPC peering connection %s for VPC %s", aws.ToString(peering.VpcPeeringConnectionId), vpcId)
+							_, err = ec2svc.DeleteVpcPeeringConnection(context.TODO(), &ec2.DeleteVpcPeeringConnectionInput{
+								VpcPeeringConnectionId: peering.VpcPeeringConnectionId,
+							})
+							if err != nil {
+								log.Printf("EKS: Warning: could not delete VPC peering connection %s: %s", aws.ToString(peering.VpcPeeringConnectionId), err)
+							}
+						}
+					}
+					// also check accepter side
+					peeringOut, err = ec2svc.DescribeVpcPeeringConnections(context.TODO(), &ec2.DescribeVpcPeeringConnectionsInput{
+						Filters: []ec2types.Filter{
+							{
+								Name:   aws.String("accepter-vpc-info.vpc-id"),
+								Values: []string{vpcId},
+							},
+						},
+					})
+					if err != nil {
+						log.Printf("EKS: Warning: could not describe accepter VPC peering connections for VPC %s: %s", vpcId, err)
+					} else {
+						for _, peering := range peeringOut.VpcPeeringConnections {
+							log.Printf("EKS: Deleting VPC peering connection %s (accepter) for VPC %s", aws.ToString(peering.VpcPeeringConnectionId), vpcId)
+							_, err = ec2svc.DeleteVpcPeeringConnection(context.TODO(), &ec2.DeleteVpcPeeringConnectionInput{
+								VpcPeeringConnectionId: peering.VpcPeeringConnectionId,
+							})
+							if err != nil {
+								log.Printf("EKS: Warning: could not delete VPC peering connection %s: %s", aws.ToString(peering.VpcPeeringConnectionId), err)
+							}
+						}
 					}
 				}
 			}
